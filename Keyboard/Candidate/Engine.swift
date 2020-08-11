87c5bbd66c164abc1d777dc2216efe23cb58c85f
Bing Jeung <bing@ososo.io>
2020-08-11 19:22:31 +0800

    Improve Engine

--- Keyboard/Candidate/Engine.swift
+++ Keyboard/Candidate/Engine.swift
@@ -13,22 +13,32 @@ struct Engine {
                 }
         }()
         
-        func suggest(for text: String) -> [Candidate] {
+        mutating func suggest(for text: String) -> [Candidate] {
                 switch text.count {
                 case 0:
                         return []
                 case 1:
                         return matchInitial(for: text)
+                case 2:
+                        canSpilt = Spliter.canSplit(text)
+                        return fetch(for: text)
+                case 3, 4, 5, 6:
+                        if !canSpilt {
+                                canSpilt = Spliter.canSplit(text)
+                        }
+                        return fetch(for: text)
                 default:
                         return fetch(for: text)
                 }
         }
         
+        private var canSpilt: Bool = false
+        
         private func fetch(for text: String) -> [Candidate] {
                 let fullMatch: [Candidate] = matchRaw(for: text)
                 guard fullMatch.count < 10 else { return fullMatch }
                 
-                guard Spliter.canSplit(text) else {
+                guard canSpilt else {
                         var combine: [Candidate] = fullMatch + matchInitial(for: text)
                         for number in 1..<text.count {
                                 combine += matchInitial(for: String(text.dropLast(number)))
@@ -40,37 +50,78 @@ struct Engine {
                 let rawJyutping: String = jyutpings.reduce("", +)
                 if text == rawJyutping {
                         var combine: [Candidate] = fullMatch
-                        if fullMatch.isEmpty {
-                                combine += suggestRawPrefix(for: text, count: 2)
-                        }
+                        var firstMatchedJyutpingCount: Int = fullMatch.isEmpty ? 0 : jyutpings.count
                         if jyutpings.count > 1 {
                                 for (number, _) in jyutpings.enumerated().reversed() {
                                         let prefix: String = jyutpings[0..<number].reduce("", +)
-                                        combine += matchRaw(for: prefix)
+                                        let matched: [Candidate] = matchRaw(for: prefix)
+                                        if combine.isEmpty && !matched.isEmpty {
+                                                firstMatchedJyutpingCount = number
+                                        }
+                                        combine += matched
+                                }
+                        }
+                        if fullMatch.isEmpty && combine.count > 2 {
+                                let tailJyutpings: [String] = Array(jyutpings.dropFirst(firstMatchedJyutpingCount))
+                                for (index, _) in tailJyutpings.enumerated().reversed() {
+                                        let someJPs: String = tailJyutpings[0...index].reduce("", +)
+                                        if let one: Candidate = matchRaw(for: someJPs, count: 1).first {
+                                                let newCandidate: Candidate = combine.first! + one
+                                                combine.insert(newCandidate, at: 0)
+                                                if combine[1].count == combine[2].count {
+                                                        let secondCandidate: Candidate = combine[2] + one
+                                                        combine.insert(secondCandidate, at: 1)
+                                                }
+                                                break
+                                        }
                                 }
                         }
                         return combine.deduplicated()
                 } else {
-                        var combine: [Candidate] = fullMatch
-                        if fullMatch.isEmpty {
-                                combine += matchRawPrefix(for: text, characterCount: jyutpings.count + 1)
-                                if (text.count - rawJyutping.count) > 1 {
-                                        let rawJPPlusOne: String = String(text.dropLast(text.count - rawJyutping.count - 1))
-                                        let jyutpingPlus: [Candidate] = matchRawPrefix(for: rawJPPlusOne, characterCount: jyutpings.count + 1)
-                                        combine += jyutpingPlus
-                                }
-                                if combine.isEmpty {
-                                        combine += suggestRawPrefix(for: text, count: 2)
-                                }
-                        }
-                        combine += matchRaw(for: rawJyutping)
+                        var matches: [Candidate] = matchRaw(for: rawJyutping)
+                        var firstMatchedJyutpingCount: Int = matches.isEmpty ? 0 : jyutpings.count
                         if jyutpings.count > 1 {
                                 for (number, _) in jyutpings.enumerated().reversed() {
                                         let prefix: String = jyutpings[0..<number].reduce("", +)
-                                        combine += matchRaw(for: prefix)
+                                        let matched: [Candidate] = matchRaw(for: prefix)
+                                        if matches.isEmpty && !matched.isEmpty {
+                                                firstMatchedJyutpingCount = number
+                                        }
+                                        matches += matched
                                 }
                         }
-                        return combine.deduplicated()
+                        if !matches.isEmpty {
+                                var hasTailCandidate: Bool = false
+                                
+                                let tailText: String = String(text.dropFirst(matches.first!.input?.count ?? 0))
+                                let tailJyutpings: [String] = Array(jyutpings.dropFirst(firstMatchedJyutpingCount))
+                                if let tailOne: Candidate = matchRawPrefix(for: tailText, characterCount: tailJyutpings.count + 1, count: 1).first {
+                                        let newCandidate: Candidate = matches.first! + tailOne
+                                        matches.insert(newCandidate, at: 0)
+                                        hasTailCandidate = true
+                                } else {
+                                        let tailRawJyutping: String = tailJyutpings.reduce("", +)
+                                        if tailText.count - tailRawJyutping.count > 1 {
+                                                let tailRawJPPlusOne: String = String(tailText.dropLast(tailText.count - tailRawJyutping.count - 1))
+                                                if let one = matchRawPrefix(for: tailRawJPPlusOne, characterCount: tailJyutpings.count + 1, count: 1).first {
+                                                        let newCandidate: Candidate = matches.first! + one
+                                                        matches.insert(newCandidate, at: 0)
+                                                        hasTailCandidate = true
+                                                }
+                                        }
+                                }
+                                if !hasTailCandidate {
+                                        for (index, _) in tailJyutpings.enumerated().reversed() {
+                                                let someJPs: String = tailJyutpings[0...index].reduce("", +)
+                                                if let one: Candidate = matchRaw(for: someJPs, count: 1).first {
+                                                        let newCandidate: Candidate = matches.first! + one
+                                                        matches.insert(newCandidate, at: 0)
+                                                        break
+                                                }
+                                        }
+                                }
+                        }
+                        return (fullMatch + matches).deduplicated()
                 }
         }
         
@@ -93,13 +144,13 @@ struct Engine {
         }
         
         // MATCH:  rawjyutping == text
-        private func matchRaw(for text: String) -> [Candidate] {
+        private func matchRaw(for text: String, count: Int = 200) -> [Candidate] {
                 guard !text.isEmpty else { return [] }
                 var candidates: [Candidate] = []
                 let queryString = "SELECT * FROM jyutpingtable WHERE rawjyutping = '\(text)\';"
                 var queryStatement: OpaquePointer? = nil
                 if sqlite3_prepare_v2(database, queryString, -1, &queryStatement, nil) == SQLITE_OK {
-                        while sqlite3_step(queryStatement) == SQLITE_ROW {
+                        while candidates.count < count && sqlite3_step(queryStatement) == SQLITE_ROW {
                                 let word: String = String(describing: String(cString: sqlite3_column_text(queryStatement, 0)))
                                 let jyutping: String = String(describing: String(cString: sqlite3_column_text(queryStatement, 1)))
                                 let candidate: Candidate = Candidate(text: word, footnote: jyutping, input: text)
