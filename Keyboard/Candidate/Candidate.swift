3b1c49f92aac5edbc475c2f84742c1a72cd65467
Bing Jeung <bing@ososo.io>
2020-08-11 17:19:13 +0800

    Add + operator

--- Keyboard/Candidate/Candidate.swift
+++ Keyboard/Candidate/Candidate.swift
@@ -27,4 +27,12 @@ struct Candidate: Hashable, CustomStringConvertible {
         func hash(into hasher: inout Hasher) {
                 hasher.combine(text)
         }
+        
+        static func + (lhs: Candidate, rhs: Candidate) -> Candidate {
+                let newText: String = lhs.text + rhs.text
+                let newFootnote: String = lhs.footnote + " " + rhs.footnote
+                let newInput: String = (lhs.input ?? "") + (rhs.input ?? "")
+                let newCandidate: Candidate = Candidate(text: newText, footnote: newFootnote, input: newInput)
+                return newCandidate
+        }
 }
