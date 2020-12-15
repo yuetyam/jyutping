c5f707d7f2dd1597b7b3256ac65c4a3ce663c1dd
Bing Jeung <bing@ososo.io>
2020-12-15 20:12:00 +0800

    Fix candidate textLabel fonts

--- Keyboard/Components/CandidateCollectionViewCell.swift
+++ Keyboard/Components/CandidateCollectionViewCell.swift
@@ -11,7 +11,8 @@ final class CandidateCollectionViewCell: UICollectionViewCell {
                 contentView.addSubview(textLabel)
                 textLabel.textAlignment = .center
                 textLabel.adjustsFontForContentSizeCategory = true
-                textLabel.font = .preferredFont(forTextStyle: .title3)
+                // textLabel.font = .preferredFont(forTextStyle: .title3)
+                textLabel.font = UIFont(name: "PingFang HK", size: 20)
                 textLabel.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
                 textLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
                 textLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
