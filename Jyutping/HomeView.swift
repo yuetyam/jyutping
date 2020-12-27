2cd4f6365c9e4d05a4837e48cc568dc0daa450d0
Bing Jeung <bing@ososo.io>
2020-12-28 05:19:57 +0800

    Check isJyutpingKeyboardEnabled before showing GuideView

--- Jyutping/HomeView.swift
+++ Jyutping/HomeView.swift
@@ -3,7 +3,41 @@ import SwiftUI
 struct HomeView: View {
         
         private let placeholder: String = NSLocalizedString("Type here to test keyboards", comment: "")
-        @State private var placeholdText: String = ""
+        @State private var cacheText: String = ""
+        
+        private var isJyutpingKeyboardEnabled: Bool {
+                guard let keyboards: [String] = UserDefaults.standard.object(forKey: "AppleKeyboards") as? [String] else { return false }
+                return keyboards.contains("im.cantonese.CantoneseIM.Keyboard")
+        }
+        
+        var body: some View {
+                NavigationView {
+                        ZStack {
+                                GlobalBackgroundColor().edgesIgnoringSafeArea(.all)
+                                ScrollView {
+                                        EnhancedTextField(placeholder: placeholder, text: $cacheText)
+                                                .padding(8)
+                                                .fillBackground()
+                                                .padding()
+                                        
+                                        if !isJyutpingKeyboardEnabled {
+                                                GuideView()
+                                        }
+                                }
+                                .navigationBarTitle(Text("Home"))
+                        }
+                }
+                .navigationViewStyle(StackNavigationViewStyle())
+        }
+}
+
+struct HomeView_Previews: PreviewProvider {
+        static var previews: some View {
+                HomeView()
+        }
+}
+
+private struct GuideView: View {
         
         private let enableKeyboard: Text = Text("•  Jump to ") + Text("Settings").fontWeight(.medium) + Text("\n") +
                 Text("•  Tap ") + Text("Keyboards").fontWeight(.medium) + Text("\n") +
@@ -17,85 +51,53 @@ struct HomeView: View {
                 Text("Add New Keyboards...").fontWeight(.medium) + Text(" or ") +
                 Text("Edit").fontWeight(.medium)
         
-        /*
-        private var isJyutpingKeyboardEnabled: Bool {
-                guard let keyboards = UserDefaults.standard.object(forKey: "AppleKeyboards") as? [String] else { return false }
-                return keyboards.contains("im.cantonese.CantoneseIM.Keyboard")
-        }
-        */
-        
         var body: some View {
-                NavigationView {
-                        ZStack {
-                                GlobalBackgroundColor().edgesIgnoringSafeArea(.all)
-                                ScrollView {
-                                        /*
-                                        TextField("Type here to test keyboards", text: $placeholdText)
-                                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                                                .padding()
-                                        */
-                                        EnhancedTextField(placeholder: placeholder, text: $placeholdText)
-                                                .padding(8)
-                                                .fillBackground()
-                                                .padding()
-                                        
-                                        VStack {
-                                                HStack {
-                                                        Text("How to enable this Keyboard").font(.system(size: 20, weight: .medium))
-                                                        Spacer()
-                                                }
-                                                .padding(.bottom)
-                                                
-                                                HStack {
-                                                        enableKeyboard.fixedSize(horizontal: false, vertical: true)
-                                                        Spacer()
-                                                }
-                                        }
-                                        .padding()
-                                        .fillBackground()
-                                        .padding()
-                                        
-                                        Button(action: {
-                                                if let url: URL = URL(string: UIApplication.openSettingsURLString) {
-                                                        UIApplication.shared.open(url)
-                                                }
-                                        }) {
-                                                HStack{
-                                                        Spacer()
-                                                        Text("Open ") + Text("Settings").fontWeight(.medium) + Text(" App")
-                                                        Spacer()
-                                                }
-                                        }
-                                        .padding()
-                                        .fillBackground()
-                                        .padding(.horizontal)
-                                        .padding(.bottom)
-                                        
-                                        VStack {
-                                                HStack {
-                                                        Text("How to add or edit keyboards").font(.system(size: 20, weight: .medium))
-                                                        Spacer()
-                                                }
-                                                .padding(.bottom)
-                                                
-                                                HStack {
-                                                        editingKeyboards.fixedSize(horizontal: false, vertical: true)
-                                                        Spacer()
-                                                }
-                                        }
-                                        .padding()
-                                        .fillBackground()
-                                        .padding()
-                                }
-                                .navigationBarTitle(Text("Home"))
+                VStack {
+                        HStack {
+                                Text("How to enable this Keyboard").font(.system(size: 20, weight: .medium))
+                                Spacer()
+                        }
+                        .padding(.bottom)
+                        
+                        HStack {
+                                enableKeyboard.fixedSize(horizontal: false, vertical: true)
+                                Spacer()
                         }
                 }
-                .navigationViewStyle(StackNavigationViewStyle())
-        }
-}
-
-struct HomeView_Previews: PreviewProvider {
-        static var previews: some View {
-                HomeView()
+                .padding()
+                .fillBackground()
+                .padding()
+                
+                Button(action: {
+                        if let url: URL = URL(string: UIApplication.openSettingsURLString) {
+                                UIApplication.shared.open(url)
+                        }
+                }) {
+                        HStack{
+                                Spacer()
+                                Text("Open ") + Text("Settings").fontWeight(.medium) + Text(" App")
+                                Spacer()
+                        }
+                }
+                .padding()
+                .fillBackground()
+                .padding(.horizontal)
+                .padding(.bottom)
+                
+                VStack {
+                        HStack {
+                                Text("How to add or edit keyboards").font(.system(size: 20, weight: .medium))
+                                Spacer()
+                        }
+                        .padding(.bottom)
+                        
+                        HStack {
+                                editingKeyboards.fixedSize(horizontal: false, vertical: true)
+                                Spacer()
+                        }
+                }
+                .padding()
+                .fillBackground()
+                .padding()
         }
 }
