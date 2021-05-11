f68f7cbba57ba8a4746133c1f636aa25b486ee53
Bing Jeung <bing@ososo.io>
2021-05-11 19:32:11 +0800

    Improve AboutView structures

--- Jyutping/Jyutping/AboutView.swift
+++ Jyutping/Jyutping/AboutView.swift
@@ -1,14 +1,12 @@
 import SwiftUI
-// import MessageUI
+
+private let version: String = {
+        let versionString: String = (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String) ?? "_error"
+        let buildString: String = (Bundle.main.infoDictionary?["CFBundleVersion"] as? String) ?? "_error"
+        return versionString + " (" + buildString + ")"
+}()
 
 struct AboutView: View {
-
-        private let versionString: String = {
-                let version: String = (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String) ?? "_error"
-                let build: String = (Bundle.main.infoDictionary?["CFBundleVersion"] as? String) ?? "_error"
-                return version + " (" + build + ")"
-        }()
-
         var body: some View {
                 NavigationView {
                         ZStack {
@@ -21,17 +19,17 @@ struct AboutView: View {
 
                                         // MARK: - Version
 
-                                        MessageView(icon: "info.circle", text: Text("Version"), message: Text(versionString))
+                                        MessageView(icon: "info.circle", text: Text("Version"), message: Text(version))
                                                 .padding(.vertical)
                                                 .fillBackground()
-                                                .contextMenu(menuItems: {
-                                                        MenuCopyButton(content: versionString)
-                                                })
+                                                .contextMenu {
+                                                        MenuCopyButton(content: version)
+                                                }
                                                 .padding()
 
 
                                         // MARK: - Source Code & Privacy
-                                        
+
                                         VStack {
                                                 LinkButton(url: URL(string: "https://github.com/yuetyam/jyutping")!,
                                                            content: MessageView(icon: "number.circle", text: Text("Source Code"), symbol: Image(systemName: "safari")))
@@ -61,10 +59,10 @@ struct AboutView: View {
                                                 }
                                         }
                                         .padding()
-                                        
-                                        
+
+
                                         // MARK: - Contact & Feedback
-                                        
+
                                         VStack {
                                                 Button(action: {
                                                         // Telegram App doesn't support Universal Links
@@ -81,16 +79,16 @@ struct AboutView: View {
                                                 Divider()
                                                 Button(action: {
                                                         // Twitter App supports Universal Links
-                                                        let twitterUrl: URL = URL(string: "https://twitter.com/JyutpingApp")!
-                                                        UIApplication.shared.open(twitterUrl)
+                                                        let url: URL = URL(string: "https://twitter.com/JyutpingApp")!
+                                                        UIApplication.shared.open(url)
                                                 }) {
                                                         MessageView(icon: "at", text: Text("Follow us on Twitter"), symbol: Image(systemName: "arrow.up.right"))
                                                 }
                                                 Divider()
                                                 Button(action: {
                                                         // GitHub App supports Universal Links
-                                                        let githubUrl: URL = URL(string: "https://github.com/yuetyam/jyutping/issues")!
-                                                        UIApplication.shared.open(githubUrl)
+                                                        let url: URL = URL(string: "https://github.com/yuetyam/jyutping/issues")!
+                                                        UIApplication.shared.open(url)
                                                 }) {
                                                         MessageView(icon: "info.circle", text: Text("GitHub Issues"), symbol: Image(systemName: "arrow.up.right"))
                                                 }
@@ -174,10 +172,10 @@ private struct EmailFeedbackButton: View {
                         UIApplication.shared.open(mailtoUrl) { success in
                                 if !success {
                                         if UITraitCollection.current.userInterfaceIdiom == .phone {
-                                                self.isMailOnPhoneUnavailable.toggle()
+                                                isMailOnPhoneUnavailable = true
                                                 UINotificationFeedbackGenerator().notificationOccurred(.warning)
                                         } else {
-                                                self.isMailOnPadUnavailable.toggle()
+                                                isMailOnPadUnavailable = true
                                         }
                                 }
                         }
@@ -200,114 +198,18 @@ private struct EmailFeedbackButton: View {
         }
 
         private let mailtoUrl: URL = {
-                let versionString: String = (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String) ?? "_error"
-                let buildString: String = (Bundle.main.infoDictionary?["CFBundleVersion"] as? String) ?? "_error"
-                let version: String = versionString + "(" + buildString + ")"
                 let device: String = UIDevice.modelName
                 let system: String = UIDevice.current.systemName + " " + UIDevice.current.systemVersion
                 let messageBody: String = """
 
-                
+
                 Version: \(version)
                 Device: \(device)
                 System: \(system)
                 """
                 let address: String = "bing@ososo.io"
-                let subject: String = "User Feedback"
+                let subject: String = "Jyutping Feedback"
                 let scheme: String = "mailto:\(address)?subject=\(subject)&body=\(messageBody)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
                 return URL(string: scheme)!
         }()
 }
-
-
-/*
-private struct MailFeedbackButton: View {
-        
-        @State private var isPresented: Bool = false
-        @State private var isMailOnPhoneUnavailable: Bool = false
-        @State private var isMailOnPadUnavailable: Bool = false
-        
-        let mailComposeDelegate: MailComposeDelegate
-
-        private let mailtoUrl: URL = {
-                let scheme: String = "mailto:bing@ososo.io?subject=User%20Feedback&body=Enter%20your%20feedback%20here"
-                return URL(string: scheme)!
-        }()
-
-        var body: some View {
-                Button(action: {
-                        if MFMailComposeViewController.canSendMail() {
-                                self.isPresented.toggle()
-                        } else if UIApplication.shared.canOpenURL(mailtoUrl) {
-                                UIApplication.shared.open(mailtoUrl)
-                        } else if UITraitCollection.current.userInterfaceIdiom == .phone {
-                                self.isMailOnPhoneUnavailable.toggle()
-                                UINotificationFeedbackGenerator().notificationOccurred(.warning)
-                        } else {
-                                self.isMailOnPadUnavailable.toggle()
-                        }
-                }) {
-                        MessageView(icon: "envelope",
-                                    text: Text("Email Feedback"),
-                                    symbol: Image(systemName: "square.and.pencil"))
-                }
-                .sheet(isPresented: $isPresented) { ComposeMailView(mailComposeDelegate: self.mailComposeDelegate) }
-                .actionSheet(isPresented: $isMailOnPhoneUnavailable) {
-                        ActionSheet(title: Text("Unable to compose mail"),
-                                    message: Text("Mail Unavailable"),
-                                    buttons: [.cancel(Text("OK"))])
-                }
-                .alert(isPresented: $isMailOnPadUnavailable) {
-                        Alert(title: Text("Unable to compose mail"),
-                              message: Text("Mail Unavailable"),
-                              dismissButton: .cancel(Text("OK"))
-                        )
-                }
-        }
-}
-private struct ComposeMailView: UIViewControllerRepresentable {
-        typealias UIViewControllerType = MFMailComposeViewController
-        let mailComposeDelegate: MailComposeDelegate
-        
-        func makeUIViewController(context: Context) -> MFMailComposeViewController {
-                let mailCompose = MFMailComposeViewController()
-                mailCompose.title = "User Feedback"
-                mailCompose.setSubject("User Feedback")
-                mailCompose.setToRecipients(["bing@ososo.io"])
-                
-                let deviceIdentifier: String = {
-                        var systemInfo = utsname()
-                        uname(&systemInfo)
-                        let machineMirror = Mirror(reflecting: systemInfo.machine)
-                        let modelIdentifier: String = machineMirror.children.reduce("") { identifier, element in
-                                guard let value: Int8 = element.value as? Int8, value != 0 else { return identifier }
-                                return identifier + String(UnicodeScalar(UInt8(value)))
-                        }
-                        return modelIdentifier
-                }()
-                let system: String = UIDevice.current.systemName + " " + UIDevice.current.systemVersion
-                let version: String = (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String) ?? "_error"
-                let build: String = (Bundle.main.infoDictionary?["CFBundleVersion"] as? String) ?? "_error"
-                let messageBody: String = """
-                [ Enter your feedback here. ]
-                
-                
-                App version: \(version), build: \(build)
-                Platform: \(deviceIdentifier) - \(system)
-                
-                """
-                mailCompose.setMessageBody(messageBody, isHTML: false)
-                
-                mailCompose.mailComposeDelegate = mailComposeDelegate
-                return mailCompose
-        }
-        func updateUIViewController(_ uiViewController: MFMailComposeViewController, context: Context) { }
-}
-private final class MailComposeDelegate: NSObject, MFMailComposeViewControllerDelegate {
-        func mailComposeController(_ controller: MFMailComposeViewController,
-                                   didFinishWith result: MFMailComposeResult,
-                                   error: Error?) {
-                controller.dismiss(animated: true, completion: nil)
-        }
-}
-*/
