6345888c5465a647c0a7faa6a9ad1035eb4f01a0
Bing Jheung <bing@ososo.io>
2021-07-05 09:12:57 +0800

    Replace Twitter, Instagram links with GETTR links

--- Jyutping/Jyutping/AboutView.swift
+++ Jyutping/Jyutping/AboutView.swift
@@ -75,37 +75,16 @@ struct AboutView: View {
                                                         }
                                                 }) {
                                                         MessageView(icon: "paperplane", text: Text("Join Telegram Group"), symbol: Image(systemName: "arrow.up.right"))
-                                                }.padding(.top)
+                                                }
                                                 Divider()
                                                 Button(action: {
-                                                        // Twitter App supports Universal Links
-                                                        let url: URL = URL(string: "https://twitter.com/JyutpingApp")!
+                                                        let url: URL = URL(string: "https://gettr.com/user/jyutping")!
                                                         UIApplication.shared.open(url)
                                                 }) {
-                                                        MessageView(icon: "at", text: Text("Follow us on Twitter"), symbol: Image(systemName: "arrow.up.right"))
+                                                        MessageView(icon: "at", text: Text("Follow us on GETTR"), symbol: Image(systemName: "arrow.up.right"))
                                                 }
-                                                Divider()
-                                                Button(action: {
-                                                        // Instagram App doesn't support Universal Links
-                                                        let appUrl: URL = URL(string: "instagram://user?username=jyutping_app")!
-                                                        let webUrl: URL = URL(string: "https://www.instagram.com/jyutping_app")!
-                                                        UIApplication.shared.open(appUrl) { success in
-                                                                if !success {
-                                                                        UIApplication.shared.open(webUrl)
-                                                                }
-                                                        }
-                                                }) {
-                                                        let icon: String = {
-                                                                if #available(iOS 14, *) {
-                                                                        return "circle.square"
-                                                                } else {
-                                                                        return "dot.square"
-                                                                }
-                                                        }()
-                                                        MessageView(icon: icon, text: Text("Follow us on Instagram"), symbol: Image(systemName: "arrow.up.right"))
-                                                }
-                                                .padding(.bottom)
                                         }
+                                        .padding(.vertical)
                                         .fillBackground()
                                         .contextMenu {
                                                 Button(action: {
@@ -114,14 +93,9 @@ struct AboutView: View {
                                                         MenuLabel(text: "Copy Telegram URL", image: "doc.on.doc")
                                                 }
                                                 Button(action: {
-                                                        UIPasteboard.general.string = "https://twitter.com/JyutpingApp"
+                                                        UIPasteboard.general.string = "https://gettr.com/user/jyutping"
                                                 }) {
-                                                        MenuLabel(text: "Copy Twitter URL", image: "doc.on.doc")
-                                                }
-                                                Button(action: {
-                                                        UIPasteboard.general.string = "https://www.instagram.com/jyutping_app"
-                                                }) {
-                                                        MenuLabel(text: "Copy Instagram URL", image: "doc.on.doc")
+                                                        MenuLabel(text: "Copy GETTR URL", image: "doc.on.doc")
                                                 }
                                         }
                                         .padding()
