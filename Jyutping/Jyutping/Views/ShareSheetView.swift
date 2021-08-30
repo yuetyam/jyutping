8b423d1f11b5c213fdb053affe4b4ab5512486c7
Bing Jheung <bing@ososo.io>
2021-08-30 21:33:07 +0800

    Implement ShareSheetView_iOS15

--- Jyutping/Jyutping/Views/ShareSheetView.swift
+++ Jyutping/Jyutping/Views/ShareSheetView.swift
@@ -40,3 +40,56 @@ private struct ActivityView: UIViewControllerRepresentable {
 
         func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
 }
+
+
+@available(iOS 15.0, *)
+struct ActivityView_iOS15: UIViewControllerRepresentable {
+
+        private let activityItems: [Any]
+        private let excludedActivityTypes: [UIActivity.ActivityType]?
+        private let completion: (() -> Void)?
+
+        init(activityItems: [Any], excludedActivityTypes: [UIActivity.ActivityType]? = nil, completion: (() -> Void)? = nil) {
+                self.activityItems = activityItems
+                self.excludedActivityTypes = excludedActivityTypes
+                self.completion = completion
+        }
+
+        private func completionHandler(activityType: UIActivity.ActivityType?, completed: Bool, returnedItems: [Any]?, activityError: Error?) {
+                completion?()
+        }
+
+        func makeUIViewController(context: Context) -> UIActivityViewController {
+                let controller = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+                controller.excludedActivityTypes = excludedActivityTypes
+                controller.completionWithItemsHandler = completionHandler
+                return controller
+        }
+
+        func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
+}
+
+@available(iOS 15.0, *)
+struct ShareSheetView_iOS15<Content: View>: View {
+
+        private let activityItems: [Any]
+        private let label: Content
+
+        init(activityItems: [Any], @ViewBuilder label: () -> Content) {
+                self.activityItems = activityItems
+                self.label = label()
+        }
+
+        @State private var isSheetPresented: Bool = false
+
+        var body: some View {
+                Button(action: {
+                        isSheetPresented = true
+                }) {
+                        label
+                }
+                .sheet(isPresented: $isSheetPresented) {
+                        ActivityView_iOS15(activityItems: activityItems)
+                }
+        }
+}
