f3424ace18ac769896a9e965fee04c4207ade3e0
Bing Jheung <bing@ososo.io>
2021-08-30 21:34:06 +0800

    Implement LinkSafariView

--- Jyutping/Jyutping/Views/LinkView.swift
+++ Jyutping/Jyutping/Views/LinkView.swift
@@ -52,3 +52,27 @@ struct LinkView_Previews: PreviewProvider {
                          url: URL(string: "https://cantonese.im")!)
         }
 }
+
+struct LinkSafariView<Content: View>: View {
+
+        private let url: URL
+        private let label: Content
+
+        init(url: URL, @ViewBuilder label: () -> Content) {
+                self.url = url
+                self.label = label()
+        }
+
+        @State private var isSafariSheetPresented: Bool = false
+
+        var body: some View {
+                Button(action: {
+                        isSafariSheetPresented = true
+                }) {
+                        label
+                }
+                .sheet(isPresented: $isSafariSheetPresented) {
+                        SafariView(url: url)
+                }
+        }
+}
