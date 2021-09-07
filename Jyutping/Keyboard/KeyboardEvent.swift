96fc0e6b596c372f92087e8ae63da96b5b8ceeb9
Bing Jheung <bing@ososo.io>
2021-09-08 07:46:51 +0800

    Improve KeyElement.init()

--- Jyutping/Keyboard/KeyboardEvent.swift
+++ Jyutping/Keyboard/KeyboardEvent.swift
@@ -24,20 +24,20 @@ struct KeySeat: Hashable {
         var hasChildren: Bool { !children.isEmpty }
 
         static let period: KeySeat = {
-                let period: KeyElement = KeyElement(text: ".")
-                let comma: KeyElement = KeyElement(text: ",")
-                let questionMark: KeyElement = KeyElement(text: "?")
-                let exclamationMark: KeyElement = KeyElement(text: "!")
+                let period: KeyElement = KeyElement(".")
+                let comma: KeyElement = KeyElement(",")
+                let questionMark: KeyElement = KeyElement("?")
+                let exclamationMark: KeyElement = KeyElement("!")
                 return KeySeat(primary: period, children: [period, comma, questionMark, exclamationMark])
         }()
         static let cantoneseComma: KeySeat = {
-                let comma: KeyElement = KeyElement(text: "，")
-                let period: KeyElement = KeyElement(text: "。")
-                let questionMark: KeyElement = KeyElement(text: "？")
-                let exclamationMark: KeyElement = KeyElement(text: "！")
+                let comma: KeyElement = KeyElement("，")
+                let period: KeyElement = KeyElement("。")
+                let questionMark: KeyElement = KeyElement("？")
+                let exclamationMark: KeyElement = KeyElement("！")
                 return KeySeat(primary: comma, children: [comma, period, questionMark, exclamationMark])
         }()
-        static let separator: KeySeat = KeySeat(primary: KeyElement(text: "'", footer: "分隔"))
+        static let separator: KeySeat = KeySeat(primary: KeyElement("'", footer: "分隔"))
 }
 
 struct KeyElement: Hashable {
@@ -46,7 +46,7 @@ struct KeyElement: Hashable {
         let header: String?
         let footer: String?
 
-        init(text: String, header: String? = nil, footer: String? = nil) {
+        init(_ text: String, header: String? = nil, footer: String? = nil) {
                 self.text = text
                 self.header = header
                 self.footer = footer
