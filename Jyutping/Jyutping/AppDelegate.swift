7c3d3cd5fdb99174dc8ebb1e8ad43383c657d886
Bing Jheung <bing@ososo.io>
2021-09-19 18:06:41 +0800

    Improve SceneDelegate.scene() structure

--- Jyutping/Jyutping/AppDelegate.swift
+++ Jyutping/Jyutping/AppDelegate.swift
@@ -18,10 +18,9 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         var window: UIWindow?
 
         func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-                let contentView = ContentView()
                 if let windowScene = scene as? UIWindowScene {
                         let window = UIWindow(windowScene: windowScene)
-                        window.rootViewController = UIHostingController(rootView: contentView)
+                        window.rootViewController = UIHostingController(rootView: ContentView())
                         self.window = window
                         window.makeKeyAndVisible()
                 }
