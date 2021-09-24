2fb1ebd9dac5412235466268660a4440d9ebe2da
Bing Jheung <bing@ososo.io>
2021-09-24 11:08:25 +0800

    Improve TabView structure

--- Jyutping/Jyutping/ContentView.swift
+++ Jyutping/Jyutping/ContentView.swift
@@ -4,23 +4,16 @@ struct ContentView: View {
         @State private var selection: Int = 0
         var body: some View {
                 TabView(selection: $selection){
-                        if #available(iOS 15.0, *) {
-                                HomeView_iOS15().tabItem {
-                                        Label("Home", systemImage: "house")
-                                }.tag(0)
-
-                                JyutpingView_iOS14().tabItem {
-                                        Label("Jyutping", systemImage: "doc.text.magnifyingglass")
-                                }.tag(1)
-
-                                AboutView_iOS14().tabItem {
-                                        Label("About", systemImage: "info.circle")
-                                }.tag(2)
-
-                        } else if #available(iOS 14.0, *) {
-                                HomeView_iOS14().tabItem {
-                                        Label("Home", systemImage: "house")
-                                }.tag(0)
+                        if #available(iOS 14.0, *) {
+                                if #available(iOS 15.0, *) {
+                                        HomeView_iOS15().tabItem {
+                                                Label("Home", systemImage: "house")
+                                        }.tag(0)
+                                } else {
+                                        HomeView_iOS14().tabItem {
+                                                Label("Home", systemImage: "house")
+                                        }.tag(0)
+                                }
 
                                 JyutpingView_iOS14().tabItem {
                                         Label("Jyutping", systemImage: "doc.text.magnifyingglass")
