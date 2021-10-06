6f372ca27f78641a325bf306b2b2d9e7b900c0eb
Bing Jheung <bing@ososo.io>
2021-10-06 10:55:32 +0800

    Implement Speaker.isLanguagesEnabled

--- Jyutping/Jyutping/Speaker.swift
+++ Jyutping/Jyutping/Speaker.swift
@@ -1,10 +1,13 @@
 import AVFoundation
 
 struct Speaker {
+
         private static let synthesizer: AVSpeechSynthesizer = AVSpeechSynthesizer()
         static func speak(_ text: String) {
                 let utterance: AVSpeechUtterance = AVSpeechUtterance(string: text)
                 utterance.voice = AVSpeechSynthesisVoice(language: "zh-HK")
                 synthesizer.speak(utterance)
         }
+
+        static let isLanguagesEnabled: Bool = Locale.preferredLanguages.contains("zh-Hant-HK")
 }
