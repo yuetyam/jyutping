c53d06dbe7224f5cec9d6998c9335d7ba509af5d
Bing Jheung <bing@ososo.io>
2021-11-18 22:33:02 +0800

    Improve AppDelegate structures

--- Jyutping/Jyutping/AppDelegate.swift
+++ Jyutping/Jyutping/AppDelegate.swift
@@ -2,21 +2,13 @@ import SwiftUI
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
-
-        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-                return true
-        }
-
         func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
                 return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
         }
 }
 
-
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
         var window: UIWindow?
-
         func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
                 if let windowScene = scene as? UIWindowScene {
                         let window = UIWindow(windowScene: windowScene)
