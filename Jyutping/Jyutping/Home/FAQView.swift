99d9e486f065dfb29723654abfec48449bf556d2
Bing Jheung <bing@ososo.io>
2021-11-23 18:25:45 +0800

    Add a new Section to FAQView

--- Jyutping/Jyutping/Home/FAQView.swift
+++ Jyutping/Jyutping/Home/FAQView.swift
@@ -8,6 +8,23 @@ struct FAQView: View {
                                 Text("Can I use with external keyboards?").font(.headline)
                                 Text("Unfortunately not. Third-party keyboard apps can't communicate with external keyboards due to system limitations.")
                         }
+                        Section {
+                                Text("What does “Allow Full Access” do?").font(.headline)
+                                if UITraitCollection.current.userInterfaceIdiom == .pad {
+                                        Text("Enable a button on the keyboard for pasting texts from Clipboard")
+                                } else {
+                                        HStack {
+                                                Text(verbatim: "1.").font(.system(size: 15, design: .monospaced))
+                                                Text("Haptic Feedback")
+                                        }
+                                        HStack {
+                                                Text(verbatim: "2.").font(.system(size: 15, design: .monospaced))
+                                                Text("Paste texts from Clipboard")
+                                        }
+                                }
+                        } footer: {
+                                Text("All other features don't require Full Access").textCase(.none)
+                        }
                 }
                 .listStyle(.insetGrouped)
                 .navigationTitle("FAQ")
