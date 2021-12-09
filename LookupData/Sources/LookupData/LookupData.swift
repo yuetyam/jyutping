5e4f68adc2b11f0729170ebb9e3d1c073e168174
Bing Zheung <bing@ososo.io>
2021-12-09 22:30:54 +0800

    Implement LookupData.advancedSearch()

--- LookupData/Sources/LookupData/LookupData.swift
+++ LookupData/Sources/LookupData/LookupData.swift
@@ -43,6 +43,58 @@ public struct LookupData {
                 }
         }
 
+        /// Search Romanization for given text.
+        ///
+        /// Would convert simplified characters to traditional.
+        ///
+        /// - Parameter text: Word to search
+        /// - Returns: Text (converted) & Array of Romanization
+        public static func advancedSearch(for text: String) -> (text: String, romanizations: [String]) {
+                lazy var fallback: (String, [String]) = (text, [])
+                guard !text.isEmpty else { return fallback }
+                if let matched: String = match(for: text) {
+                        let romanizations: [String] = matched.components(separatedBy: ".")
+                        return (text, romanizations)
+                } else {
+                        let traditionalText: String = convert(from: text)
+                        let tryMatched: String = match(for: traditionalText) ?? ""
+                        guard tryMatched.isEmpty else {
+                                let romanizations: [String] = tryMatched.components(separatedBy: ".")
+                                return (traditionalText, romanizations)
+                        }
+                        guard text.count != 1 else { return fallback }
+
+                        lazy var chars: String = text
+                        lazy var fetches: [String] = []
+                        lazy var newText: String = ""
+                        while !chars.isEmpty {
+                                let leading = fetchLeading(for: chars)
+                                lazy var traditionalChars: String = convert(from: chars)
+                                lazy var tryLeading = fetchLeading(for: traditionalChars)
+                                if let romanization: String = leading.romanization {
+                                        fetches.append(romanization)
+                                        let length: Int = max(1, leading.charCount)
+                                        newText += chars.dropLast(chars.count - length)
+                                        chars = String(chars.dropFirst(length))
+                                } else if let tryRomanization: String = tryLeading.romanization {
+                                        fetches.append(tryRomanization)
+                                        let length: Int = max(1, tryLeading.charCount)
+                                        newText += traditionalChars.dropLast(traditionalChars.count - length)
+                                        chars = String(chars.dropFirst(length))
+                                } else {
+                                        if let first = chars.first {
+                                                newText += String(first)
+                                        }
+                                        fetches.append("?")
+                                        chars = String(chars.dropFirst())
+                                }
+                        }
+                        guard !fetches.isEmpty else { return fallback }
+                        let suggestion: String = fetches.joined(separator: " ")
+                        return (newText, [suggestion])
+                }
+        }
+
         private static func fetchLeading(for word: String) -> (romanization: String?, charCount: Int) {
                 var chars: String = word
                 var romanization: String? = nil
@@ -74,5 +126,13 @@ public struct LookupData {
                         }
                 }
                 return nil
+        }
+
+        /// Convert simplified characters to traditional
+        /// - Parameter text: Simplified characters
+        /// - Returns: Traditional characters
+        private static func convert(from text: String) -> String {
+                let transformed: String? = text.applyingTransform(StringTransform("Simplified-Traditional"), reverse: false)
+                return transformed ?? text
         }
 }
