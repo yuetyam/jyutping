b3520b9646d3261432a3141bc1849187b57193e0
Bing Zheung <bing@ososo.io>
2022-01-06 16:59:19 +0800

    Implement Speech.stop() and Speech.isSpeaking

--- Jyutping/Jyutping/Speech.swift
+++ Jyutping/Jyutping/Speech.swift
@@ -6,7 +6,17 @@ struct Speech {
         static func speak(_ text: String) {
                 let utterance: AVSpeechUtterance = AVSpeechUtterance(string: text)
                 utterance.voice = AVSpeechSynthesisVoice(language: "zh-HK")
-                synthesizer.speak(utterance)
+                DispatchQueue.main.async {
+                        synthesizer.speak(utterance)
+                }
+        }
+        static func stop() {
+                DispatchQueue.main.async {
+                        synthesizer.stopSpeaking(at: .immediate)
+                }
+        }
+        static var isSpeaking: Bool {
+                return synthesizer.isSpeaking
         }
 
         static let isLanguagesEnabled: Bool = Locale.preferredLanguages.contains("zh-Hant-HK")
