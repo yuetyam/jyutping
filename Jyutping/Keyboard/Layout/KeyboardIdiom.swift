5e302393972a6939e37f6a2ff4bca76eb8e39863
Bing Zheung <bing@ososo.io>
2022-02-11 10:56:51 +0800

    Move enum KeyboardLayout to a separate file

--- Jyutping/Keyboard/Layout/KeyboardIdiom.swift
+++ Jyutping/Keyboard/Layout/KeyboardIdiom.swift
@@ -42,17 +42,3 @@ extension KeyboardIdiom {
                 }
         }
 }
-
-
-/// 鍵盤佈局
-enum KeyboardLayout {
-
-        /// 全鍵盤
-        case qwerty
-
-        /// 三拼
-        case saamPing
-
-        /// 九宮格
-        case grid
-}
---
+++ Jyutping/Keyboard/Layout/KeyboardLayout.swift
@@ -0,0 +1,12 @@
+/// 鍵盤佈局
+enum KeyboardLayout {
+
+        /// 全鍵盤
+        case qwerty
+
+        /// 三拼
+        case saamPing
+
+        /// 九宮格
+        case grid
+}
