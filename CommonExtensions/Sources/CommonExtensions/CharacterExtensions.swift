1ccf066f6faf71bee202d8eb527eec1b2aac63eb
Bing Zheung <bing@ososo.io>
2022-03-22 19:19:57 +0800

    (CommonExtensions) Add CharacterExtensions.swift

---
+++ CommonExtensions/Sources/CommonExtensions/CharacterExtensions.swift
@@ -0,0 +1,34 @@
+extension Character {
+
+        /// UNICODE code points. Example: Á = ["U+41", "U+301"]
+        public var codePoints: [String] {
+                return self.unicodeScalars.map { "U+" + String($0.value, radix: 16, uppercase: true) }
+        }
+
+        /// UNICODE code points as a String. Example: Á = "U+41 U+301"
+        public var codePointsText: String {
+                return self.codePoints.joined(separator: " ")
+        }
+
+        /// Create a Character from the given Unicode Code Point String (U+XXXX)
+        public init?(codePoint: String) {
+                let cropped = codePoint.trimmingCharacters(in: .whitespacesAndNewlines).replacingOccurrences(of: "U+", with: "", options: [.anchored, .caseInsensitive])
+                guard let u32 = UInt32(cropped, radix: 16) else { return nil }
+                guard let scalar = Unicode.Scalar(u32) else { return nil }
+                self.init(scalar)
+        }
+
+
+        /// UNICODE code point as decimal code
+        public var decimalCode: Int? {
+                guard let scalar = self.unicodeScalars.first else { return nil }
+                let number = Int(scalar.value)
+                return number
+        }
+
+        /// Create a Character from the given Unicode code point (decimal)
+        public init?(decimal: Int) {
+                guard let scalar = Unicode.Scalar(decimal) else { return nil }
+                self.init(scalar)
+        }
+}
