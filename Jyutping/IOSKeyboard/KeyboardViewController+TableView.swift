8d30f468c6b5639fb809ac752324ad52c33282c7
Bing Zheung <bing@ososo.io>
2022-04-27 07:29:58 +0800

    (iOS Keyboard) Improve Jyutping Tones Display settings texts

--- Jyutping/IOSKeyboard/KeyboardViewController+TableView.swift
+++ Jyutping/IOSKeyboard/KeyboardViewController+TableView.swift
@@ -168,16 +168,16 @@ extension KeyboardViewController: UITableViewDataSource, UITableViewDelegate {
                         let cell = tableView.dequeueReusableCell(withIdentifier: Identifiers.selectionSettingsCell, for: indexPath)
                         switch indexPath.row {
                         case 0:
-                                cell.textLabel?.text = NSLocalizedString("Normal : jyut6 ping3", comment: .empty)
+                                cell.textLabel?.text = NSLocalizedString("TonesDisplayStyle1", comment: .empty)
                                 cell.accessoryType = (toneStyle < 2) ? .checkmark : .none
                         case 1:
-                                cell.textLabel?.text = NSLocalizedString("No Tones : jyut ping", comment: .empty)
+                                cell.textLabel?.text = NSLocalizedString("TonesDisplayStyle2", comment: .empty)
                                 cell.accessoryType = (toneStyle == 2) ? .checkmark : .none
                         case 2:
-                                cell.textLabel?.text = NSLocalizedString("Superscript : jyut⁶ ping³", comment: .empty)
+                                cell.textLabel?.text = NSLocalizedString("TonesDisplayStyle3", comment: .empty)
                                 cell.accessoryType = (toneStyle == 3) ? .checkmark : .none
                         case 3:
-                                cell.textLabel?.text = NSLocalizedString("Subscript : jyut₆ ping₃", comment: .empty)
+                                cell.textLabel?.text = NSLocalizedString("TonesDisplayStyle4", comment: .empty)
                                 cell.accessoryType = (toneStyle == 4) ? .checkmark : .none
                         default:
                                 break
