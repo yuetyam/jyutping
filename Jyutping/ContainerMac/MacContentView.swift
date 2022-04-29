ad6503ca1cc6c1eef6a1cc6f9499ba6e1b0fc5e7
Bing Zheung <bing@ososo.io>
2022-04-29 13:40:06 +0800

    Improve macOS Container App structures

--- Jyutping/ContainerMac/MacContentView.swift
+++ Jyutping/ContainerMac/MacContentView.swift
@@ -5,17 +5,50 @@ struct MacContentView: View {
                 NavigationView {
                         List {
                                 Section {
-                                        NavigationLink(destination: Text("Home")) {
-                                                Label("Home", systemImage: "house")
+                                        NavigationLink(destination: Text("Introductions")) {
+                                                Label("Introductions", systemImage: "book")
                                         }
+                                        NavigationLink(destination: Text("Home")) {
+                                                Label("label.title.expressions", systemImage: "text.quote")
+                                        }
+                                } header: {
+                                        Text("Keyboard").textCase(.none)
                                 }
                                 Section {
                                         NavigationLink(destination: Text("About")) {
-                                                Label("About", systemImage: "info.circle")
+                                                Label("Search", systemImage: "magnifyingglass")
                                         }
+                                        NavigationLink(destination: Text("About")) {
+                                                Label("Initials", systemImage: "rectangle.leadingthird.inset.filled")
+                                        }
+                                        NavigationLink(destination: Text("About")) {
+                                                Label("Finals", systemImage: "rectangle.trailingthird.inset.filled")
+                                        }
+                                        NavigationLink(destination: Text("About")) {
+                                                Label("Tones", systemImage: "bell")
+                                        }
+                                        NavigationLink(destination: Text("About")) {
+                                                Label("Resources", systemImage: "square.3.stack.3d")
+                                        }
+                                } header: {
+                                        Text("Jyutping").textCase(.none)
+                                }
+                                Section {
+                                        NavigationLink(destination: Text("About")) {
+                                                Label("About", systemImage: "globe.asia.australia")
+                                        }
+                                        NavigationLink(destination: Text("About")) {
+                                                Label("FAQ", systemImage: "questionmark.circle")
+                                        }
+                                        NavigationLink(destination: Text("About")) {
+                                                Label("Version", systemImage: "info.circle")
+                                        }
+                                } header: {
+                                        Text("About").textCase(.none)
                                 }
                         }
-                        .navigationTitle("Home")
+                        .listStyle(.sidebar)
+                        .navigationTitle("Jyutping")
                 }
         }
 }
