657ed905e8cefc469441b974d65d0caceedb8aa1
Bing Zheung <bing@ososo.io>
2022-05-08 03:31:09 +0800

    (macOS App) Improve MacSearchView

--- Jyutping/ContainerMac/MacSearchView.swift
+++ Jyutping/ContainerMac/MacSearchView.swift
@@ -9,58 +9,54 @@ struct MacSearchView: View {
         @State private var pronunciations: [String] = []
 
         var body: some View {
-                List {
-                        Section {
-                                HStack {
-                                        Image(systemName: "magnifyingglass")
-                                        TextField("Lookup Jyutping for Cantonese", text: $inputText)
-                                                .disableAutocorrection(true)
-                                                .onSubmit {
-                                                        submittedText = inputText.trimmed()
-                                                        guard submittedText != cantonese else { return }
-                                                        guard !submittedText.isEmpty else {
-                                                                cantonese = submittedText
-                                                                pronunciations = []
-                                                                return
-                                                        }
-                                                        let search = AppMaster.lookup(text: submittedText)
-                                                        if search.romanizations.isEmpty {
-                                                                cantonese = submittedText
-                                                                pronunciations = []
-                                                        } else {
-                                                                cantonese = search.text
-                                                                pronunciations = search.romanizations
-                                                        }
+                ScrollView {
+                        LazyVStack(spacing: 16) {
+                                TextField("Lookup Jyutping for Cantonese", text: $inputText)
+                                        .textFieldStyle(.roundedBorder)
+                                        .disableAutocorrection(true)
+                                        .onSubmit {
+                                                submittedText = inputText.trimmed()
+                                                guard submittedText != cantonese else { return }
+                                                guard !submittedText.isEmpty else {
+                                                        cantonese = .empty
+                                                        pronunciations = []
+                                                        return
                                                 }
-                                }
-                        }
-                        if !submittedText.isEmpty && pronunciations.isEmpty {
-                                Section {
-                                        Text("No Results.")
-                                        Text("Common Cantonese words only.").font(.footnote)
-                                }
-                        } else if !submittedText.isEmpty {
-                                Section {
+                                                let search = AppMaster.lookup(text: submittedText)
+                                                if search.romanizations.isEmpty {
+                                                        cantonese = submittedText
+                                                        pronunciations = []
+                                                } else {
+                                                        cantonese = search.text
+                                                        pronunciations = search.romanizations
+                                                }
+                                        }
+                                        .padding(.vertical)
+                                if !cantonese.isEmpty {
                                         HStack {
-                                                Text(verbatim: cantonese)
+                                                Text(verbatim: cantonese).font(.title3)
                                                 Spacer()
                                                 Speaker(cantonese)
                                         }
-                                        ForEach(pronunciations, id: \.self) { romanization in
-                                                HStack(spacing: 16) {
-                                                        Text(verbatim: romanization)
-                                                        if cantonese.count == 1 {
-                                                                Text(verbatim: Syllable2IPA.IPAText(romanization)).foregroundColor(.secondary)
-                                                        }
-                                                        Spacer()
-                                                        Speaker(romanization)
-                                                }
-                                        }
+                                        .block()
                                 }
-                                .textSelection(.enabled)
+                                ForEach(0..<pronunciations.count, id: \.self) { index in
+                                        let romanization: String = pronunciations[index]
+                                        HStack(spacing: 16) {
+                                                Text(verbatim: romanization).font(.title3)
+                                                if cantonese.count == 1 {
+                                                        Text(verbatim: Syllable2IPA.IPAText(romanization)).foregroundColor(.secondary)
+                                                }
+                                                Spacer()
+                                                Speaker(romanization)
+                                        }
+                                        .block()
+                                }
                         }
+                        .textSelection(.enabled)
+                        .padding(32)
                 }
-                .animation(.default, value: pronunciations)
+                .animation(.default, value: cantonese)
                 .navigationTitle("Search")
         }
 }
