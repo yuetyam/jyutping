f6345bc802dea9da0fc0f170bc5fad575248617e
Bing Zheung <bing@ososo.io>
2022-05-10 17:19:48 +0800

    (iOS App) Improve ExpressionsView

--- Jyutping/ContainerIOS/Home/ExpressionsView.swift
+++ Jyutping/ContainerIOS/Home/ExpressionsView.swift
@@ -5,12 +5,7 @@ struct ExpressionsView: View {
                 List {
                         Group {
                                 Section {
-                                        Label {
-                                                Text(verbatim: "第二人稱代詞")
-                                        } icon: {
-                                                Image(systemName: "1.circle")
-                                        }
-                                        .font(.headline)
+                                        Text(verbatim: "第二人稱代詞").font(.masterHeadline)
 
                                         Label {
                                                 Text(verbatim: "單數：你")
@@ -34,12 +29,7 @@ struct ExpressionsView: View {
                                         }
                                 }
                                 Section {
-                                        Label {
-                                                Text(verbatim: "第三人稱代詞")
-                                        } icon: {
-                                                Image(systemName: "2.circle")
-                                        }
-                                        .font(.headline)
+                                        Text(verbatim: "第三人稱代詞").font(.masterHeadline)
 
                                         Label {
                                                 Text(verbatim: "單數：佢")
@@ -65,118 +55,120 @@ struct ExpressionsView: View {
                         }
                         Group {
                                 Section {
-                                        Label {
-                                                Text(verbatim: "區分「係」同「喺」")
-                                        } icon: {
-                                                Image(systemName: "3.circle")
+                                        Text(verbatim: "區分「係」同「喺」").font(.masterHeadline)
+                                        HStack {
+                                                Text(verbatim: "係")
+                                                HStack(spacing: 2) {
+                                                        Text(verbatim: "hai6").font(.fixedWidth)
+                                                        Speaker("hai6")
+                                                }
+                                                Text(verbatim: "謂語，義同是")
+                                                Spacer()
                                         }
-                                        .font(.headline)
-                                        Label {
-                                                Text(verbatim: """
-                                                係 hai6：謂語，義同是。
-                                                喺 hai2：表方位、時間，義同在。
-                                                """)
-                                        } icon: {
-                                                Image(systemName: "info.circle").hidden()
+                                        HStack {
+                                                Text(verbatim: "喺")
+                                                HStack(spacing: 2) {
+                                                        Text(verbatim: "hai2").font(.fixedWidth)
+                                                        Speaker("hai2")
+                                                }
+                                                Text(verbatim: "表方位、時間，義同在")
+                                                Spacer()
                                         }
-                                        Label {
-                                                Text(verbatim: """
-                                                例：我係曹阿瞞。
-                                                例：我喺天后站落車。
-                                                """)
-                                        } icon: {
-                                                Image.checkmark.foregroundColor(.green).hidden()
+                                        HStack {
+                                                Text(verbatim: "例：我係曹阿瞞。")
+                                                Speaker("我係曹阿瞞。")
+                                                Spacer()
+                                        }
+                                        HStack {
+                                                Text(verbatim: "例：我喺天后站落車。")
+                                                Speaker("我喺天后站落車。")
+                                                Spacer()
                                         }
                                 }
-                                .lineSpacing(5)
-
                                 Section {
-                                        Label {
-                                                Text(verbatim: "區分「諗」同「冧」")
-                                        } icon: {
-                                                Image(systemName: "4.circle")
+                                        Text(verbatim: "區分「諗」同「冧」").font(.masterHeadline)
+                                        HStack(spacing: 16) {
+                                                Text(verbatim: "諗")
+                                                HStack(spacing: 2) {
+                                                        Text(verbatim: "nam2").font(.fixedWidth)
+                                                        Speaker("nam2")
+                                                }
+                                                Text(verbatim: "想、思考、覺得")
+                                                Spacer()
                                         }
-                                        .font(.headline)
-                                        Label {
-                                                Text(verbatim: """
-                                                諗 nam2：想、思考、覺得。
-                                                冧 lam3：表示倒塌、倒下。
-                                                """)
-                                        } icon: {
-                                                Image(systemName: "info.circle").hidden()
+                                        HStack(spacing: 16) {
+                                                Text(verbatim: "冧")
+                                                HStack(spacing: 2) {
+                                                        Text(verbatim: "lam3").font(.fixedWidth)
+                                                        Speaker("lam3")
+                                                }
+                                                Text(verbatim: "表示倒塌、倒下")
+                                                Spacer()
                                         }
-                                        Label {
-                                                Text(verbatim: """
-                                                例：我諗緊今晚食咩。
-                                                例：佢畀人㨃冧咗。
-                                                """)
-                                        } icon: {
-                                                Image.checkmark.foregroundColor(.green).hidden()
+                                        HStack {
+                                                Text(verbatim: "例：我諗緊今晚食咩。")
+                                                Speaker("我諗緊今晚食咩。")
+                                                Spacer()
+                                        }
+                                        HStack {
+                                                Text(verbatim: "例：佢畀人㨃冧咗。")
+                                                Speaker("佢畀人㨃冧咗。")
+                                                Spacer()
                                         }
                                 }
-                                .lineSpacing(5)
-
                                 Section {
-                                        Label {
-                                                Text(verbatim: "區分「咁」同「噉」")
-                                        } icon: {
-                                                Image(systemName: "5.circle")
+                                        Text(verbatim: "區分「咁」同「噉」").font(.masterHeadline)
+                                        HStack(spacing: 16) {
+                                                Text(verbatim: "咁")
+                                                HStack(spacing: 2) {
+                                                        Text(verbatim: "gam3").font(.fixedWidth)
+                                                        Speaker("gam3")
+                                                }
+                                                Text(verbatim: "音同「禁」")
+                                                Spacer()
                                         }
-                                        .font(.headline)
-
-                                        Label {
-                                                Text(verbatim: """
-                                                咁 gam3，音同「禁」。
-                                                噉 gam2，音同「感」。
-                                                """)
-                                        } icon: {
-                                                Image(systemName: "info.circle").hidden()
+                                        HStack(spacing: 16) {
+                                                Text(verbatim: "噉")
+                                                HStack(spacing: 2) {
+                                                        Text(verbatim: "gam2").font(.fixedWidth)
+                                                        Speaker("gam2")
+                                                }
+                                                Text(verbatim: "音同「感」")
+                                                Spacer()
                                         }
-                                        Label {
-                                                Text(verbatim: """
-                                                例：我生得咁靚仔。
-                                                例：噉又未必。
-                                                """)
-                                        } icon: {
-                                                Image.checkmark.foregroundColor(.green).hidden()
+                                        HStack {
+                                                Text(verbatim: "例：我生得咁靚仔。")
+                                                Speaker("我生得咁靚仔。")
+                                                Spacer()
+                                        }
+                                        HStack {
+                                                Text(verbatim: "例：噉又未必。")
+                                                Speaker("gam2 又未必。")
+                                                Spacer()
                                         }
                                 }
-                                .lineSpacing(5)
                         }
                         Group {
                                 Section {
-                                        Label {
-                                                HStack {
-                                                        Text(verbatim: "推薦").fontWeight(.medium)
-                                                        Text(verbatim: "嘅／個得噉。")
-                                                        Text(verbatim: "避免").fontWeight(.medium)
-                                                        Text(verbatim: "的得地")
-                                                }
-                                        } icon: {
-                                                Image(systemName: "6.circle").font(.headline)
+                                        HStack {
+                                                Text(verbatim: "推薦").fontWeight(.medium)
+                                                Text(verbatim: "嘅／個得噉。")
+                                                Text(verbatim: "避免").fontWeight(.medium)
+                                                Text(verbatim: "的得地")
+                                                Spacer()
                                         }
-                                        Label {
-                                                Text(verbatim: """
-                                                例：我嘅細佬／我個細佬。
-                                                例：講得好！
-                                                例：細細聲噉講話。
-                                                """)
-                                        } icon: {
-                                                Image.checkmark.foregroundColor(.green).hidden()
-                                        }
+                                        Text(verbatim: "例：我嘅細佬／我個細佬。")
+                                        Text(verbatim: "例：講得好！")
+                                        Text(verbatim: "例：細細聲噉講話。")
                                 }
-                                .lineSpacing(5)
 
                                 Section {
-                                        Label {
-                                                HStack {
-                                                        Text(verbatim: "推薦").fontWeight(.medium)
-                                                        Text(verbatim: "啩、啊嘛。")
-                                                        Text(verbatim: "避免").fontWeight(.medium)
-                                                        Text(verbatim: "吧")
-                                                }
-                                        } icon: {
-                                                Image(systemName: "7.circle").font(.headline)
+                                        HStack {
+                                                Text(verbatim: "推薦").fontWeight(.medium)
+                                                Text(verbatim: "啩、啊嘛。")
+                                                Text(verbatim: "避免").fontWeight(.medium)
+                                                Text(verbatim: "吧")
+                                                Spacer()
                                         }
                                         Label {
                                                 Text(verbatim: "下個禮拜會出啩。")
@@ -200,15 +192,12 @@ struct ExpressionsView: View {
                                         }
                                 }
                                 Section {
-                                        Label {
-                                                HStack {
-                                                        Text(verbatim: "推薦").fontWeight(.medium)
-                                                        Text(verbatim: "啦、嘞。")
-                                                        Text(verbatim: "避免").fontWeight(.medium)
-                                                        Text(verbatim: "了")
-                                                }
-                                        } icon: {
-                                                Image(systemName: "8.circle").font(.headline)
+                                        HStack {
+                                                Text(verbatim: "推薦").fontWeight(.medium)
+                                                Text(verbatim: "啦、嘞。")
+                                                Text(verbatim: "避免").fontWeight(.medium)
+                                                Text(verbatim: "了")
+                                                Spacer()
                                         }
                                         Label {
                                                 Text(verbatim: "各位，我毋客氣啦。")
@@ -232,15 +221,12 @@ struct ExpressionsView: View {
                                         }
                                 }
                                 Section {
-                                        Label {
-                                                HStack {
-                                                        Text(verbatim: "推薦").fontWeight(.medium)
-                                                        Text(verbatim: "使。")
-                                                        Text(verbatim: "避免").fontWeight(.medium)
-                                                        Text(verbatim: "駛、洗")
-                                                }
-                                        } icon: {
-                                                Image(systemName: "9.circle").font(.headline)
+                                        HStack {
+                                                Text(verbatim: "推薦").fontWeight(.medium)
+                                                Text(verbatim: "使。")
+                                                Text(verbatim: "避免").fontWeight(.medium)
+                                                Text(verbatim: "駛、洗")
+                                                Spacer()
                                         }
                                         Label {
                                                 Text(verbatim: "毋使驚")
@@ -259,15 +245,12 @@ struct ExpressionsView: View {
                                         }
                                 }
                                 Section {
-                                        Label {
-                                                HStack {
-                                                        Text(verbatim: "推薦").fontWeight(.medium)
-                                                        Text(verbatim: "而家／而今。")
-                                                        Text(verbatim: "避免").fontWeight(.medium)
-                                                        Text(verbatim: "宜家")
-                                                }
-                                        } icon: {
-                                                Image(systemName: "10.circle").font(.headline)
+                                        HStack {
+                                                Text(verbatim: "推薦").fontWeight(.medium)
+                                                Text(verbatim: "而家／而今。")
+                                                Text(verbatim: "避免").fontWeight(.medium)
+                                                Text(verbatim: "宜家")
+                                                Spacer()
                                         }
                                         Label {
                                                 Text(verbatim: "我而家食緊飯。")
@@ -282,10 +265,9 @@ struct ExpressionsView: View {
                                 }
                         }
                 }
-                .navigationTitle("title.expressions")
-                #if os(iOS)
+                .font(.master)
                 .listStyle(.insetGrouped)
+                .navigationTitle("title.expressions")
                 .navigationBarTitleDisplayMode(.inline)
-                #endif
         }
 }
