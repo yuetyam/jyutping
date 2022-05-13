6d19d40717b3bf52881d76d44a6a5c9b76324089
Bing Zheung <bing@ososo.io>
2022-05-13 23:19:37 +0800

    (Containers) Improve View.block()

--- Jyutping/ContainerShared/Extensions/ViewExtensions.swift
+++ Jyutping/ContainerShared/Extensions/ViewExtensions.swift
@@ -2,12 +2,10 @@ import SwiftUI
 
 extension View {
 
-        @available(macOS 12.0, *)
         @available(iOS, unavailable)
         func block() -> some View {
                 #if os(macOS)
-                let color: Color = Color(nsColor: NSColor.textBackgroundColor)
-                return self.padding().background(color, in: RoundedRectangle(cornerRadius: 8, style: .continuous))
+                return self.padding().background(Color.textBackgroundColor, in: RoundedRectangle(cornerRadius: 8, style: .continuous))
                 #else
                 return self
                 #endif
