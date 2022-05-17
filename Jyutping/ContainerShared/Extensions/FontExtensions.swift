a806350c48865d8dea69943818ff342a7fe1c572
Bing Zheung <bing@ososo.io>
2022-05-17 13:46:33 +0800

    (Containers) Improve FontExtensions

--- Jyutping/ContainerShared/Extensions/FontExtensions.swift
+++ Jyutping/ContainerShared/Extensions/FontExtensions.swift
@@ -11,41 +11,61 @@ extension Font {
         }()
 
         static let master: Font = {
-                let size: CGFloat = {
-                        #if os(iOS)
-                        return 17
-                        #else
-                        return 13
-                        #endif
-                }()
-                return Font.custom(primaryFontName, size: size, relativeTo: .body)
+                #if os(iOS)
+                return Font.body
+                #else
+                return generateFont(size: 13, style: .body)
+                #endif
         }()
-
         static let masterHeadline: Font = {
                 #if os(iOS)
-                return Font.custom(primaryFontName, size: 17, relativeTo: .body).weight(.medium)
+                return Font.body.weight(.medium)
                 #else
-                return Font.custom(primaryFontName, size: 15, relativeTo: .title3)
+                return generateFont(size: 15, style: .title3)
                 #endif
         }()
 
-        private static let primaryFontName: String = {
-                let preferredList: [String] = ["ChiuKong Gothic CL", "Source Han Sans K", "Noto Sans CJK KR"]
-                let fontName: String = {
+        #if os(macOS)
+        private static func generateFont(size: CGFloat, style: Font.TextStyle) -> Font {
+                let primaryFontName: String = {
+                        let preferredList: [String] = ["ChiuKong Gothic CL", "Source Han Sans K", "Noto Sans CJK KR"]
                         for name in preferredList {
-                                #if os(iOS)
-                                if let font = UIFont(name: name, size: 17) {
+                                if let _ = NSFont(name: name, size: size) {
                                         return name
                                 }
-                                #else
-                                if let font = NSFont(name: name, size: 13) {
-                                        return name
-                                }
-                                #endif
                         }
                         return "PingFang HK"
                 }()
-                return fontName
-        }()
-}
+                let fallbackFontNames: [String] = {
+                        let expected: [String] = ["I.MingCP", "I.Ming", "HanaMinB"]
+                        let results: [String?] = expected.map { name -> String? in
+                                if let _ = NSFont(name: name, size: size) {
+                                        return name
+                                } else {
+                                        return nil
+                                }
+                        }
+                        let found: [String] = results.compactMap({ $0 })
+                        return found
+                }()
+                if fallbackFontNames.isEmpty {
+                        return Font.custom(primaryFontName, size: size, relativeTo: style)
+                } else {
+                        return pairFonts(primary: primaryFontName, fallbacks: fallbackFontNames, size: size)
+                }
+        }
+        #endif
 
+        #if os(macOS)
+        private static func pairFonts(primary name: String, fallbacks: [String], size: CGFloat) -> Font {
+                let originalFont: NSFont = NSFont(name: name, size: size) ?? .systemFont(ofSize: size)
+                let originalDescriptor: NSFontDescriptor = originalFont.fontDescriptor
+                let fallbackDescriptors: [NSFontDescriptor] = fallbacks.map { fontName -> NSFontDescriptor in
+                        return originalDescriptor.addingAttributes([.name: fontName])
+                }
+                let pairedDescriptor: NSFontDescriptor = originalDescriptor.addingAttributes([.cascadeList : fallbackDescriptors])
+                let pairedFont: NSFont = NSFont(descriptor: pairedDescriptor, size: size) ?? .systemFont(ofSize: size)
+                return Font(pairedFont)
+        }
+        #endif
+}
