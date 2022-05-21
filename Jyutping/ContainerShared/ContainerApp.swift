729f4ad717d268dbac1733e7147796173ad62788
Bing Zheung <bing@ososo.io>
2022-05-21 08:19:52 +0800

    (macOS App) Implement applicationShouldTerminateAfterLastWindowClosed()

--- Jyutping/ContainerShared/ContainerApp.swift
+++ Jyutping/ContainerShared/ContainerApp.swift
@@ -1,7 +1,23 @@
 import SwiftUI
 
+
+#if os(macOS)
+import AppKit
+class AppDelegate: NSObject, NSApplicationDelegate {
+        func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+                return true
+        }
+}
+#endif
+
+
 @main
 struct ContainerApp: App {
+
+        #if os(macOS)
+        @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+        #endif
+
         var body: some Scene {
                 WindowGroup {
                         #if os(iOS)
