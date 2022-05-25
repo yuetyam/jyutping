7f1ab4300c8b132fedded14df06aff167eebfa8f
Bing Zheung <bing@ososo.io>
2022-05-25 16:38:33 +0800

    (iOS App) Add Configuration.storekit

--- Jyutping/ContainerIOS/About/DonationView.swift
+++ Jyutping/ContainerIOS/About/DonationView.swift
@@ -1,46 +1,124 @@
 import SwiftUI
+import StoreKit
+
+@available(iOS 15.0, *)
+private class DonateViewModel: ObservableObject {
+
+        @Published private(set) var products: [Product] = []
+
+        init() {
+                checkUpdates()
+        }
+
+        func fetchProducts() {
+                Task {
+                        let products = try await Product.products(for: [
+                                DonationID.supportAuthor,
+                                DonationID.supportAuthorPlus,
+                                DonationID.supportAuthorMax
+                        ])
+                        DispatchQueue.main.async {
+                                self.products = products
+                        }
+                }
+        }
+
+        func purchase(id: String) {
+                Task {
+                        guard let product = products.filter({ $0.id == id }).first else { return }
+                        let result = try await product.purchase()
+                        switch result {
+                        case .success(let verification):
+                                switch verification {
+                                case .verified(let transaction):
+                                        await transaction.finish()
+                                case .unverified:
+                                        break
+                                }
+                        case .pending:
+                                break
+                        case .userCancelled:
+                                break
+                        @unknown default:
+                                break
+                        }
+                }
+        }
+
+        private func checkUpdates() {
+                Task(priority: .background) {
+                        for await _ in Transaction.updates {
+                        }
+                }
+        }
+}
+
+
+private struct DonationID {
+        static let supportAuthor: String = "im.cantonese.CantoneseIM.support.author"
+        static let supportAuthorPlus: String = "im.cantonese.CantoneseIM.support.author.plus"
+        static let supportAuthorMax: String = "im.cantonese.CantoneseIM.support.author.max"
+}
+
 
 @available(iOS 15.0, *)
 struct DonationView: View {
+
+        @StateObject private var donateViewModel: DonateViewModel = DonateViewModel()
 
         var body: some View {
                 List {
                         Section {
                                 Button {
-                                        print("IAP")
+                                        donateViewModel.purchase(id: DonationID.supportAuthor)
                                 } label: {
                                         HStack {
                                                 Spacer()
-                                                Text("Support Author")
-                                                Text(verbatim: "$0.99")
+                                                if let item = donateViewModel.products.filter({ $0.id == DonationID.supportAuthor }).first {
+                                                        Text(verbatim: item.displayName)
+                                                        Text(verbatim: item.displayPrice)
+                                                } else {
+                                                        Text("Support Author")
+                                                }
                                                 Spacer()
                                         }
                                 }
                         }
                         Section {
                                 Button {
-                                        print("IAP")
+                                        donateViewModel.purchase(id: DonationID.supportAuthorPlus)
                                 } label: {
                                         HStack {
                                                 Spacer()
-                                                Text("Support Author Plus")
-                                                Text(verbatim: "$1.99")
+                                                if let item = donateViewModel.products.filter({ $0.id == DonationID.supportAuthorPlus }).first {
+                                                        Text(verbatim: item.displayName)
+                                                        Text(verbatim: item.displayPrice)
+                                                } else {
+                                                        Text("Support Author Plus")
+                                                }
                                                 Spacer()
                                         }
                                 }
                         }
                         Section {
                                 Button {
-                                        print("IAP")
+                                        donateViewModel.purchase(id: DonationID.supportAuthorMax)
                                 } label: {
                                         HStack {
                                                 Spacer()
-                                                Text("Support Author Max")
-                                                Text(verbatim: "$5.99")
+                                                if let item = donateViewModel.products.filter({ $0.id == DonationID.supportAuthorMax }).first {
+                                                        Text(verbatim: item.displayName)
+                                                        Text(verbatim: item.displayPrice)
+                                                } else {
+                                                        Text("Support Author Max")
+                                                }
                                                 Spacer()
                                         }
                                 }
                         }
+                }
+                .task {
+                        donateViewModel.fetchProducts()
                 }
                 .navigationTitle("Thank You")
                 .navigationBarTitleDisplayMode(.inline)
