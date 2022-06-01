622e1e3f1d1700d04858beb0cbc2dcf7f3fd2f15
Bing Zheung <bing@ososo.io>
2022-06-02 07:11:53 +0800

    (MacIME) Fix InputMethodConnectionName

--- Jyutping/Jyutping/JyutpingApp.swift
+++ Jyutping/Jyutping/JyutpingApp.swift
@@ -20,7 +20,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
         var server: IMKServer?
 
         func applicationDidFinishLaunching(_ notification: Notification) {
-                let name: String = (Bundle.main.infoDictionary?["InputMethodConnectionName"] as? String) ?? "org.jyutping.inputmethod.Jyutping_Connection"
+                let name: String = (Bundle.main.infoDictionary?["InputMethodConnectionName"] as? String) ?? "Jyutping_1_Connection"
                 server = IMKServer(name: name, bundleIdentifier: Bundle.main.bundleIdentifier)
         }
 }
