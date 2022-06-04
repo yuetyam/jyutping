107d527ba29be77fc5e61aa6d6f19f3d0325fef3
Bing Zheung <bing@ososo.io>
2022-06-04 09:13:14 +0800

    (CoreIME) Remove a typealias

--- Modules/CoreIME/Sources/CoreIME/Engine.swift
+++ Modules/CoreIME/Sources/CoreIME/Engine.swift
@@ -3,8 +3,7 @@ import SQLite3
 
 extension Lychee {
 
-        fileprivate typealias Candidate = CoreCandidate
-        fileprivate typealias RowCandidate = (candidate: Candidate, row: Int)
+        fileprivate typealias RowCandidate = (candidate: CoreCandidate, row: Int)
 
         public static func suggest(for text: String, schemes: [[String]]) -> [CoreCandidate] {
                 let startsWithY: Bool = text.hasPrefix("y")
@@ -23,17 +22,17 @@ extension Lychee {
                 }
         }
 
-        private static func fetchTwoChars(_ text: String) -> [Candidate] {
+        private static func fetchTwoChars(_ text: String) -> [CoreCandidate] {
                 guard let firstChar = text.first, let lastChar = text.last else { return [] }
                 guard !(firstChar.isSeparator || firstChar.isTone) else { return [] }
                 guard !lastChar.isSeparator else { return match(for: String(firstChar)) }
-                let matched: [Candidate] = match(for: text)
+                let matched: [CoreCandidate] = match(for: text)
                 guard !lastChar.isTone else { return matched }
-                let shortcutTwo: [Candidate] = shortcut(for: text)
-                let shortcutFirst: [Candidate] = shortcut(for: String(firstChar))
+                let shortcutTwo: [CoreCandidate] = shortcut(for: text)
+                let shortcutFirst: [CoreCandidate] = shortcut(for: String(firstChar))
                 return matched + shortcutTwo + shortcutFirst
         }
-        private static func fetchThreeChars(_ text: String) -> [Candidate] {
+        private static func fetchThreeChars(_ text: String) -> [CoreCandidate] {
                 guard let firstChar = text.first, let lastChar = text.last else { return [] }
                 guard !(firstChar.isSeparator || firstChar.isTone) else { return [] }
                 let medium = text[text.index(text.startIndex, offsetBy: 1)]
@@ -49,8 +48,8 @@ extension Lychee {
                         guard !(lastChar.isSeparator || lastChar.isTone) else {
                                 return match(for: leadingTwo)
                         }
-                        let fetches: [Candidate] = prefix(match: headTail)
-                        let filtered: [Candidate] = fetches.filter {
+                        let fetches: [CoreCandidate] = prefix(match: headTail)
+                        let filtered: [CoreCandidate] = fetches.filter {
                                 guard let first: String = $0.romanization.components(separatedBy: String.space).first else { return false }
                                 return first == leadingTwo
                         }
@@ -61,30 +60,30 @@ extension Lychee {
                 } else if lastChar.isTone {
                         return match(for: text)
                 }
-                let exactly: [Candidate] = match(for: text)
-                let prefixes: [Candidate] = prefix(match: text)
-                let shortcutThree: [Candidate] = shortcut(for: text)
-                let shortcutTwo: [Candidate] = shortcut(for: leadingTwo)
-                let matchTwo: [Candidate] = match(for: leadingTwo)
-                let shortcutFirst: [Candidate] = shortcut(for: String(firstChar))
+                let exactly: [CoreCandidate] = match(for: text)
+                let prefixes: [CoreCandidate] = prefix(match: text)
+                let shortcutThree: [CoreCandidate] = shortcut(for: text)
+                let shortcutTwo: [CoreCandidate] = shortcut(for: leadingTwo)
+                let matchTwo: [CoreCandidate] = match(for: leadingTwo)
+                let shortcutFirst: [CoreCandidate] = shortcut(for: String(firstChar))
 
-                var combine: [Candidate] = []
-                if let shortcutLast1: Candidate = shortcut(for: String(lastChar), count: 1).first {
-                        if let firstMatchTwo: Candidate = matchTwo.first {
-                                let new: Candidate = firstMatchTwo + shortcutLast1
+                var combine: [CoreCandidate] = []
+                if let shortcutLast1: CoreCandidate = shortcut(for: String(lastChar), count: 1).first {
+                        if let firstMatchTwo: CoreCandidate = matchTwo.first {
+                                let new: CoreCandidate = firstMatchTwo + shortcutLast1
                                 combine.append(new)
                         }
-                        if let firstShortcutTwo: Candidate = matchTwo.first {
-                                let new: Candidate = firstShortcutTwo + shortcutLast1
+                        if let firstShortcutTwo: CoreCandidate = matchTwo.first {
+                                let new: CoreCandidate = firstShortcutTwo + shortcutLast1
                                 combine.append(new)
                         }
                 }
-                let head: [Candidate] = exactly + prefixes + shortcutThree
-                let tail: [Candidate] = combine + shortcutTwo + matchTwo + shortcutFirst
+                let head: [CoreCandidate] = exactly + prefixes + shortcutThree
+                let tail: [CoreCandidate] = combine + shortcutTwo + matchTwo + shortcutFirst
                 return head + tail
         }
 
-        private static func fetch(for text: String, origin: String, schemes: [[String]]) -> [Candidate] {
+        private static func fetch(for text: String, origin: String, schemes: [[String]]) -> [CoreCandidate] {
                 guard let bestScheme: [String] = schemes.first, !bestScheme.isEmpty else {
                         return processUnsplittable(text)
                 }
@@ -96,15 +95,15 @@ extension Lychee {
                 }
         }
 
-        private static func processUnsplittable(_ text: String) -> [Candidate] {
-                var combine: [Candidate] = match(for: text) + prefix(match: text) + shortcut(for: text)
+        private static func processUnsplittable(_ text: String) -> [CoreCandidate] {
+                var combine: [CoreCandidate] = match(for: text) + prefix(match: text) + shortcut(for: text)
                 for number in 1..<text.count {
                         let leading: String = String(text.dropLast(number))
                         combine += shortcut(for: leading)
                 }
                 return combine
         }
-        private static func process(text: String, origin: String, sequences: [[String]]) -> [Candidate] {
+        private static func process(text: String, origin: String, sequences: [[String]]) -> [CoreCandidate] {
                 let hasSeparators: Bool = text.count != origin.count
                 let candidates = match(schemes: sequences, hasSeparators: hasSeparators)
                 guard !hasSeparators else { return candidates }
@@ -120,37 +119,37 @@ extension Lychee {
                 let combines = qualified.map({ $0.element + backCandidate })
                 return combines + candidates
         }
-        private static func processPartial(text: String, origin: String, sequences: [[String]]) -> [Candidate] {
+        private static func processPartial(text: String, origin: String, sequences: [[String]]) -> [CoreCandidate] {
                 let hasSeparators: Bool = text.count != origin.count
-                let candidates: [Candidate] = match(schemes: sequences, hasSeparators: hasSeparators)
-                lazy var fallback: [Candidate] = match(for: text) + prefix(match: text, count: 5) + candidates + shortcut(for: text)
+                let candidates: [CoreCandidate] = match(schemes: sequences, hasSeparators: hasSeparators)
+                lazy var fallback: [CoreCandidate] = match(for: text) + prefix(match: text, count: 5) + candidates + shortcut(for: text)
                 guard !hasSeparators else { return fallback }
-                guard let firstCandidate: Candidate = candidates.first else { return fallback }
+                guard let firstCandidate: CoreCandidate = candidates.first else { return fallback }
                 let firstInputCount: Int = firstCandidate.input.count
                 guard firstInputCount != text.count else { return fallback }
                 let tailText: String = String(text.dropFirst(firstInputCount))
-                if let tailOne: Candidate = prefix(match: tailText, count: 1).first {
-                        let combine: Candidate = firstCandidate + tailOne
+                if let tailOne: CoreCandidate = prefix(match: tailText, count: 1).first {
+                        let combine: CoreCandidate = firstCandidate + tailOne
                         return match(for: text) + prefix(match: text, count: 5) + [combine] + candidates + shortcut(for: text)
                 }
                 let tailSyllables: [String] = Splitter.peekSplit(tailText)
                 guard !(tailSyllables.isEmpty) else { return fallback }
-                var concatenated: [Candidate] = []
+                var concatenated: [CoreCandidate] = []
                 let hasTailCandidate: Bool = {
                         let syllablesText = tailSyllables.joined()
                         let difference: Int = tailText.count - syllablesText.count
                         guard difference > 1 else { return false }
                         let syllablesPlusOne = tailText.dropLast(difference - 1)
                         guard let one = prefix(match: String(syllablesPlusOne), count: 1).first else { return false }
-                        let combine: Candidate = firstCandidate + one
+                        let combine: CoreCandidate = firstCandidate + one
                         concatenated.append(combine)
                         return true
                 }()
                 if !hasTailCandidate {
                         for (index, _) in tailSyllables.enumerated().reversed() {
                                 let someSyllables: String = tailSyllables[0...index].joined()
-                                if let one: Candidate = matchWithLimitCount(for: someSyllables, count: 1).first {
-                                        let combine: Candidate = firstCandidate + one
+                                if let one: CoreCandidate = matchWithLimitCount(for: someSyllables, count: 1).first {
+                                        let combine: CoreCandidate = firstCandidate + one
                                         concatenated.append(combine)
                                         break
                                 }
@@ -158,13 +157,13 @@ extension Lychee {
                 }
                 return match(for: text) + prefix(match: text, count: 5) + concatenated + candidates + shortcut(for: text)
         }
-        private static func match(schemes: [[String]], hasSeparators: Bool) -> [Candidate] {
+        private static func match(schemes: [[String]], hasSeparators: Bool) -> [CoreCandidate] {
                 let matches = schemes.map({ matchWithRowID(for: $0.joined()) }).joined()
                 let sorted = matches.sorted { $0.candidate.text.count == $1.candidate.text.count && ($1.row - $0.row) > 50000 }
-                let candidates: [Candidate] = sorted.map({ $0.candidate })
+                let candidates: [CoreCandidate] = sorted.map({ $0.candidate })
                 guard hasSeparators else { return candidates }
                 let firstSyllable: String = schemes.first?.first ?? "X"
-                let filtered: [Candidate] = candidates.filter { candidate in
+                let filtered: [CoreCandidate] = candidates.filter { candidate in
                         let firstRomanization: String = candidate.romanization.components(separatedBy: String.space).first ?? "Y"
                         return firstSyllable == firstRomanization.removedTones()
                 }
@@ -177,17 +176,17 @@ private extension Lychee {
 
         // CREATE TABLE imetable(word TEXT NOT NULL, romanization TEXT NOT NULL, ping INTEGER NOT NULL, shortcut INTEGER NOT NULL, prefix INTEGER NOT NULL);
 
-        static func shortcut(for text: String, count: Int = 100) -> [Candidate] {
+        static func shortcut(for text: String, count: Int = 100) -> [CoreCandidate] {
                 guard !text.isEmpty else { return [] }
                 let textHash: Int = text.replacingOccurrences(of: "y", with: "j").hash
-                var candidates: [Candidate] = []
+                var candidates: [CoreCandidate] = []
                 let queryString = "SELECT word, romanization FROM imetable WHERE shortcut = \(textHash) LIMIT \(count);"
                 var queryStatement: OpaquePointer? = nil
                 if sqlite3_prepare_v2(Lychee.database, queryString, -1, &queryStatement, nil) == SQLITE_OK {
                         while sqlite3_step(queryStatement) == SQLITE_ROW {
                                 let word: String = String(cString: sqlite3_column_text(queryStatement, 0))
                                 let romanization: String = String(cString: sqlite3_column_text(queryStatement, 1))
-                                let candidate: Candidate = Candidate(text: word, romanization: romanization, input: text)
+                                let candidate: CoreCandidate = CoreCandidate(text: word, romanization: romanization, input: text)
                                 candidates.append(candidate)
                         }
                 }
@@ -195,9 +194,9 @@ private extension Lychee {
                 return candidates
         }
 
-        static func match(for text: String) -> [Candidate] {
+        static func match(for text: String) -> [CoreCandidate] {
                 guard !text.isEmpty else { return [] }
-                var candidates: [Candidate] = []
+                var candidates: [CoreCandidate] = []
                 let tones: String = text.tones
                 let hasTones: Bool = !tones.isEmpty
                 let ping: String = hasTones ? text.removedTones() : text
@@ -208,7 +207,7 @@ private extension Lychee {
                                 let word: String = String(cString: sqlite3_column_text(queryStatement, 0))
                                 let romanization: String = String(cString: sqlite3_column_text(queryStatement, 1))
                                 if !hasTones || tones == romanization.tones || (tones.count == 1 && text.last == romanization.last) {
-                                        let candidate: Candidate = Candidate(text: word, romanization: romanization, input: text)
+                                        let candidate: CoreCandidate = CoreCandidate(text: word, romanization: romanization, input: text)
                                         candidates.append(candidate)
                                 }
                         }
@@ -216,9 +215,9 @@ private extension Lychee {
                 sqlite3_finalize(queryStatement)
                 return candidates
         }
-        static func matchWithLimitCount(for text: String, count: Int) -> [Candidate] {
+        static func matchWithLimitCount(for text: String, count: Int) -> [CoreCandidate] {
                 guard !text.isEmpty else { return [] }
-                var candidates: [Candidate] = []
+                var candidates: [CoreCandidate] = []
                 let tones: String = text.tones
                 let hasTones: Bool = !tones.isEmpty
                 let ping: String = hasTones ? text.removedTones() : text
@@ -229,7 +228,7 @@ private extension Lychee {
                                 let word: String = String(cString: sqlite3_column_text(queryStatement, 0))
                                 let romanization: String = String(cString: sqlite3_column_text(queryStatement, 1))
                                 if !hasTones || tones == romanization.tones || (tones.count == 1 && text.last == romanization.last) {
-                                        let candidate: Candidate = Candidate(text: word, romanization: romanization, input: text)
+                                        let candidate: CoreCandidate = CoreCandidate(text: word, romanization: romanization, input: text)
                                         candidates.append(candidate)
                                 }
                         }
@@ -251,7 +250,7 @@ private extension Lychee {
                                 let word: String = String(cString: sqlite3_column_text(queryStatement, 1))
                                 let romanization: String = String(cString: sqlite3_column_text(queryStatement, 2))
                                 if !hasTones || tones == romanization.tones || (tones.count == 1 && text.last == romanization.last) {
-                                        let candidate: Candidate = Candidate(text: word, romanization: romanization, input: text)
+                                        let candidate: CoreCandidate = CoreCandidate(text: word, romanization: romanization, input: text)
                                         let rowCandidate: RowCandidate = (candidate: candidate, row: rowid)
                                         rowCandidates.append(rowCandidate)
                                 }
@@ -261,16 +260,16 @@ private extension Lychee {
                 return rowCandidates
         }
 
-        static func prefix(match text: String, count: Int = 100) -> [Candidate] {
+        static func prefix(match text: String, count: Int = 100) -> [CoreCandidate] {
                 guard !text.isEmpty else { return [] }
-                var candidates: [Candidate] = []
+                var candidates: [CoreCandidate] = []
                 let queryString = "SELECT word, romanization FROM imetable WHERE prefix = \(text.hash) LIMIT \(count);"
                 var queryStatement: OpaquePointer? = nil
                 if sqlite3_prepare_v2(Lychee.database, queryString, -1, &queryStatement, nil) == SQLITE_OK {
                         while sqlite3_step(queryStatement) == SQLITE_ROW {
                                 let word: String = String(cString: sqlite3_column_text(queryStatement, 0))
                                 let romanization: String = String(cString: sqlite3_column_text(queryStatement, 1))
-                                let candidate: Candidate = Candidate(text: word, romanization: romanization, input: text)
+                                let candidate: CoreCandidate = CoreCandidate(text: word, romanization: romanization, input: text)
                                 candidates.append(candidate)
                         }
                 }
