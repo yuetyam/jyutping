cd30aa82a0e287d75e3fd0184840e48b04673263
Bing Zheung <bing@ososo.io>
2022-09-19 09:30:52 +0800

    (Container) Update AVSpeechSynthesisVoice for iOS 16

--- Jyutping/ContainerShared/Speech.swift
+++ Jyutping/ContainerShared/Speech.swift
@@ -3,8 +3,20 @@ import AVFoundation
 struct Speech {
 
         private static let synthesizer: AVSpeechSynthesizer = AVSpeechSynthesizer()
-        private static let enhancedVoice: AVSpeechSynthesisVoice? = AVSpeechSynthesisVoice(identifier: "com.apple.ttsbundle.Sin-Ji-premium")
-        private static let voice: AVSpeechSynthesisVoice? = enhancedVoice ?? AVSpeechSynthesisVoice(language: "zh-HK")
+        private static let voice: AVSpeechSynthesisVoice? = {
+                if let iOS16Premium = AVSpeechSynthesisVoice(identifier: "com.apple.voice.premium.zh-HK.Sinji") {
+                        return iOS16Premium
+                } else if let oldIOSEnhanced = AVSpeechSynthesisVoice(identifier: "com.apple.ttsbundle.Sin-Ji-premium") {
+                        return oldIOSEnhanced
+                } else if let iOS16Enhanced = AVSpeechSynthesisVoice(identifier: "com.apple.voice.enhanced.zh-HK.Sinji") {
+                        return iOS16Enhanced
+                } else if let possibleOldIOSEnhanced = AVSpeechSynthesisVoice(identifier: "com.apple.ttsbundle.Sin-Ji-enhanced") {
+                        // Not sure this identifier valid or not
+                        return possibleOldIOSEnhanced
+                } else {
+                        return AVSpeechSynthesisVoice(language: "zh-HK")
+                }
+        }()
 
         static func speak(_ text: String) {
                 let utterance: AVSpeechUtterance = AVSpeechUtterance(string: text)
@@ -23,5 +35,17 @@ struct Speech {
         }
 
         static let isLanguagesEnabled: Bool = Locale.preferredLanguages.contains("zh-Hant-HK")
-        static let isEnhancedVoiceAvailable: Bool = enhancedVoice != nil
+        static let isEnhancedVoiceAvailable: Bool = {
+                guard let voiceQuality = voice?.quality else { return false }
+                switch voiceQuality {
+                case .default:
+                        return false
+                case .enhanced:
+                        return true
+                case .premium:
+                        return true
+                @unknown default:
+                        return true
+                }
+        }()
 }
