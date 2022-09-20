ca3ca34b965507174412e4c747a0e002c589a5b4
Bing Zheung <bing@ososo.io>
2022-09-20 10:21:52 +0800

    (packaging) Update jyutping.rb for v0.20.0

--- packaging/jyutping.rb
+++ packaging/jyutping.rb
@@ -1,6 +1,6 @@
 cask "jyutping" do
-  version "0.19.0"
-  sha256 "a8444ba17624820f3d4152561dc93cd8265911c217f08897088017875f832426"
+  version "0.20.0"
+  sha256 "da6ba7e31340706fba137773256030df8b4b1bf25ee7c06edea30c40bf3d3762"
 
   url "https://github.com/yuetyam/jyutping/releases/download/#{version}/Jyutping-IME-Mac-v#{version}.zip",
       verified: "github.com/yuetyam/jyutping/"
