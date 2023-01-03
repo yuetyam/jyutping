feb8f3ddbd6c7c70799ecf9449dbb74b84a1aa4b
Bing Zheung <bing@ososo.io>
2023-01-03 14:36:04 +0800

    (CommonExtensions) Implement extension Character.isIdeographic

--- Modules/CommonExtensions/Sources/CommonExtensions/CharacterExtensions.swift
+++ Modules/CommonExtensions/Sources/CommonExtensions/CharacterExtensions.swift
@@ -37,4 +37,47 @@ extension Character {
         public var isBasicLatinLetter: Bool {
                 return ("a"..."z") ~= self || ("A"..."Z") ~= self
         }
+
+
+        /// is CJKV character
+        public var isIdeographic: Bool {
+                guard let scalarValue: UInt32 = self.unicodeScalars.first?.value else { return false }
+                switch scalarValue {
+                case 0x3007:
+                        return true
+                case 0x4E00...0x9FFF:
+                        return true
+                case 0x3400...0x4DBF:
+                        return true
+                case 0x20000...0x2A6DF:
+                        return true
+                case 0x2A700...0x2B73F:
+                        return true
+                case 0x2B740...0x2B81F:
+                        return true
+                case 0x2B820...0x2CEAF:
+                        return true
+                case 0x2CEB0...0x2EBEF:
+                        return true
+                case 0x30000...0x3134F:
+                        return true
+                case 0x31350...0x323AF:
+                        return true
+                default:
+                        return false
+                }
+        }
 }
+
+/*
+U+3007: 〇
+U+4E00-U+9FFF: CJK Unified Ideographs
+U+3400-U+4DBF: CJK Unified Ideographs Extension A
+U+20000-U+2A6DF: CJK Unified Ideographs Extension B
+U+2A700-U+2B73F: CJK Unified Ideographs Extension C
+U+2B740-U+2B81F: CJK Unified Ideographs Extension D
+U+2B820-U+2CEAF: CJK Unified Ideographs Extension E
+U+2CEB0-U+2EBEF: CJK Unified Ideographs Extension F
+U+30000-U+3134F: CJK Unified Ideographs Extension G
+U+31350-U+323AF: CJK Unified Ideographs Extension H
+*/
