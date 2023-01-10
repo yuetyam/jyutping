349c8121f480c617bb38ad4ed2b4c8111b8ac394
Bing Zheung <bing@ososo.io>
2023-01-10 20:33:12 +0800

    (Jyutping App) Implement extension Text.separator

--- Jyutping/Jyutping/Extensions/TextExtensions.swift
+++ Jyutping/Jyutping/Extensions/TextExtensions.swift
@@ -1,5 +1,10 @@
 import SwiftUI
 
 extension Text {
+
+        /// `Text(verbatim: "•")`
         static let dotMark: Text = Text(verbatim: "•")
+
+        /// `Text(verbatim: ": ")`
+        static let separator: Text = Text(verbatim: ": ").foregroundColor(.separator)
 }
