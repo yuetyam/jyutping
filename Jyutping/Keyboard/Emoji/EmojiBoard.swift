acbdff425112a6829bc8b5acd9445c8e0bf733bd
Bing Zheung <bing@ososo.io>
2023-01-13 20:19:07 +0800

    (iOS Keyboard) Improve Emoji keyboard indicators

--- Jyutping/Keyboard/Emoji/EmojiBoard.swift
+++ Jyutping/Keyboard/Emoji/EmojiBoard.swift
@@ -7,14 +7,7 @@ final class EmojiBoard: UIView {
         let indicatorsStackView: UIStackView = UIStackView()
 
         init() {
-                let imageName: String = {
-                        if #available(iOSApplicationExtension 14.0, *) {
-                                return "abc"
-                        } else {
-                                return "textformat.abc"
-                        }
-                }()
-                backButton = Indicator(index: -1, imageName: imageName)
+                backButton = Indicator(index: -1, imageName: "abc")
                 backspaceKey = Indicator(index: 9, imageName: "delete.left")
                 super.init(frame: .zero)
                 setupBackButton()
@@ -46,14 +39,21 @@ final class EmojiBoard: UIView {
                 ])
         }
         private func setupSubViews() {
+                let footballImageName: String = {
+                        if #available(iOSApplicationExtension 16.0, *) {
+                                return "soccerball"
+                        } else {
+                                return "EmojiCategoryActivity"
+                        }
+                }()
                 let indicator_0: Indicator = Indicator(index: 0, imageName: "clock")
                 let indicator_1: Indicator = Indicator(index: 1, imageName: "EmojiSmiley")
-                let indicator_2: Indicator = Indicator(index: 2, imageName: "tortoise")
-                let indicator_3: Indicator = Indicator(index: 3, imageName: "EmojiICategoryFoodAndDrink")
-                let indicator_4: Indicator = Indicator(index: 4, imageName: "EmojiICategoryActivity")
-                let indicator_5: Indicator = Indicator(index: 5, imageName: "EmojiICategoryTravelAndPlaces")
+                let indicator_2: Indicator = Indicator(index: 2, imageName: "hare")
+                let indicator_3: Indicator = Indicator(index: 3, imageName: "EmojiCategoryFoodAndDrink")
+                let indicator_4: Indicator = Indicator(index: 4, imageName: footballImageName)
+                let indicator_5: Indicator = Indicator(index: 5, imageName: "EmojiCategoryTravelAndPlaces")
                 let indicator_6: Indicator = Indicator(index: 6, imageName: "lightbulb")
-                let indicator_7: Indicator = Indicator(index: 7, imageName: "EmojiICategorySymbols")
+                let indicator_7: Indicator = Indicator(index: 7, imageName: "EmojiCategorySymbols")
                 let indicator_8: Indicator = Indicator(index: 8, imageName: "flag")
                 indicatorsStackView.distribution = .fillProportionally
                 indicatorsStackView.addArrangedSubviews([indicator_0, indicator_1, indicator_2, indicator_3, indicator_4, indicator_5, indicator_6, indicator_7, indicator_8])
@@ -80,23 +80,34 @@ final class Indicator: UIButton {
                 let indicatorImageView: UIImageView = UIImageView()
                 addSubview(indicatorImageView)
                 indicatorImageView.translatesAutoresizingMaskIntoConstraints = false
+                let baseInset: CGFloat = 20
                 let topInset: CGFloat = {
                         switch index {
+                        case 2:
+                                return baseInset + 2
+                        case 3:
+                                return baseInset + 2
                         case 4:
-                                return 13
+                                if #available(iOSApplicationExtension 16.0, *) {
+                                        return baseInset
+                                } else {
+                                        return baseInset + 3
+                                }
                         case 5:
-                                return 12
+                                return baseInset + 2
+                        case 6:
+                                return baseInset + 1
                         case 7:
-                                return 14
+                                return baseInset + 4
                         case 9:
-                                return 10
+                                return baseInset - 2
                         default:
-                                return 11
+                                return baseInset
                         }
                 }()
                 NSLayoutConstraint.activate([
                         indicatorImageView.topAnchor.constraint(equalTo: topAnchor, constant: topInset),
-                        indicatorImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+                        indicatorImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
                         indicatorImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
                         indicatorImageView.trailingAnchor.constraint(equalTo: trailingAnchor)
                 ])
