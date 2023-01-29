d435f3f1b6adc8d816d883687ffcf7aa86764f5d
Bing Zheung <bing@ososo.io>
2023-01-30 02:03:43 +0800

    (Jyutping App) Improve FontExtensions

--- Jyutping/Jyutping/Extensions/FontExtensions.swift
+++ Jyutping/Jyutping/Extensions/FontExtensions.swift
@@ -4,82 +4,73 @@ extension Font {
 
         static let fixedWidth: Font = Font.body.monospaced()
 
-        static let master: Font = {
-                #if os(iOS)
-                return Font.body
-                #else
-                return constructFont(size: 14)
-                #endif
-        }()
-
-        /// Heading
-        static let significant: Font = {
-                #if os(iOS)
-                return Font.body.weight(.medium)
-                #else
-                return constructFont(size: 16)
-                #endif
-        }()
-
-        /// Subheadline
-        static let copilot: Font = {
-                #if os(iOS)
-                return Font.subheadline
-                #else
-                return constructFont(size: 12)
-                #endif
-        }()
-
-        #if os(macOS)
-        private static func constructFont(size: CGFloat) -> Font {
-                let isSFProAvailable: Bool = found(font: Constant.SFPro)
-                let primary: String = isSFProAvailable ? Constant.SFPro : Constant.HelveticaNeue
-                let fallbacks: [String] = {
-                        var list: [String] = isSFProAvailable ? [Constant.HelveticaNeue] : []
-                        let firstWave: [String] = ["ChiuKong Gothic CL", "Advocate Ancient Sans", "Source Han Sans K", "Noto Sans CJK KR", "Sarasa Gothic CL"]
-                        for name in firstWave {
-                                if found(font: name) {
-                                        list.append(name)
-                                        break
-                                }
-                        }
-                        list.append(Constant.PingFangHK)
-                        let planFonts: [String] = ["Plangothic P1", "Plangothic P2"]
-                        for name in planFonts {
-                                if found(font: name) {
-                                        list.append(name)
-                                }
-                        }
-                        let IMingFonts: [String] = [Constant.IMingCP, Constant.IMing]
-                        for name in IMingFonts {
-                                if found(font: name) {
-                                        list.append(name)
-                                        break
-                                }
-                        }
-                        if found(font: Constant.HanaMinB) {
-                                list.append(Constant.HanaMinB)
-                        }
-                        return list
-                }()
-                let shouldUseSystemFont: Bool = fallbacks == [Constant.PingFangHK]
-                guard !shouldUseSystemFont else { return Font.system(size: size) }
-                return pairFonts(primary: primary, fallbacks: fallbacks, size: size)
-        }
-
-        private static func found(font fontName: String) -> Bool {
-                return NSFont(name: fontName, size: 15) != nil
-        }
-
-        private static func pairFonts(primary name: String, fallbacks: [String], size: CGFloat) -> Font {
-                let originalFont: NSFont = NSFont(name: name, size: size) ?? .systemFont(ofSize: size)
-                let originalDescriptor: NSFontDescriptor = originalFont.fontDescriptor
-                let fallbackDescriptors: [NSFontDescriptor] = fallbacks.map { fontName -> NSFontDescriptor in
-                        return originalDescriptor.addingAttributes([.name: fontName])
-                }
-                let pairedDescriptor: NSFontDescriptor = originalDescriptor.addingAttributes([.cascadeList : fallbackDescriptors])
-                let pairedFont: NSFont = NSFont(descriptor: pairedDescriptor, size: size) ?? .systemFont(ofSize: size)
-                return Font(pairedFont)
-        }
+        #if os(iOS)
+        static let master: Font = Font.body
+        static let significant: Font = Font.body.weight(.medium)
+        static let copilot: Font = Font.subheadline
+        #else
+        static let master: Font = enhancedFont(size: 14)
+        static let significant: Font = enhancedFont(size: 16)
+        static let copilot: Font = enhancedFont(size: 12)
         #endif
 }
+
+#if os(macOS)
+
+private extension Font {
+
+        static func enhancedFont(size: CGFloat) -> Font {
+                let shouldUseSystemFont: Bool = fontNames.count == 2
+                guard !shouldUseSystemFont else { return Font.system(size: size) }
+                return combinedFont(from: fontNames, size: size)
+        }
+
+        private static let fontNames: [String] = {
+                var names: [String] = []
+                if found(font: Constant.SFPro) {
+                        names.append(Constant.SFPro)
+                }
+                names.append(Constant.HelveticaNeue)
+                let expected: [String] = ["ChiuKong Gothic CL", "Advocate Ancient Sans", "Source Han Sans K", "Noto Sans CJK KR", "Sarasa Gothic CL"]
+                for name in expected {
+                        if found(font: name) {
+                                names.append(name)
+                                break
+                        }
+                }
+                names.append(Constant.PingFangHK)
+                let planFonts: [String] = ["Plangothic P1", "Plangothic P2"]
+                for name in planFonts where found(font: name) {
+                        names.append(name)
+                }
+                let IMingFonts: [String] = [Constant.IMingCP, Constant.IMing]
+                for name in IMingFonts {
+                        if found(font: name) {
+                                names.append(name)
+                                break
+                        }
+                }
+                if found(font: Constant.HanaMinB) {
+                        names.append(Constant.HanaMinB)
+                }
+                return names
+        }()
+
+        private static func found(font name: String) -> Bool {
+                return NSFont(name: name, size: 15) != nil
+        }
+
+        private static func combinedFont(from names: [String], size: CGFloat) -> Font {
+                guard let primary = names.first, let primaryFont = NSFont(name: primary, size: size) else { return Font.system(size: size) }
+                let fallbacks: [String] = names.dropFirst().compactMap({ $0 })
+                let primaryDescriptor: NSFontDescriptor = primaryFont.fontDescriptor
+                let descriptors: [NSFontDescriptor] = fallbacks.map { name -> NSFontDescriptor in
+                        return primaryDescriptor.addingAttributes([.name: name])
+                }
+                let descriptor: NSFontDescriptor = primaryDescriptor.addingAttributes([.cascadeList : descriptors])
+                guard let combined: NSFont = NSFont(descriptor: descriptor, size: size) else { return Font.system(size: size) }
+                return Font(combined)
+        }
+}
+
+#endif
