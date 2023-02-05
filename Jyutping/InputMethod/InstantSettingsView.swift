fac2c4802ced4c0c765fbb0e472fc31f3c41fe24
Bing Zheung <bing@ososo.io>
2023-02-05 17:12:22 +0800

    (InputMethod) Improve InstantSettingsView UI

--- Jyutping/InputMethod/InstantSettingsView.swift
+++ Jyutping/InputMethod/InstantSettingsView.swift
@@ -25,26 +25,30 @@ struct InstantSettingsView: View {
 
         private let verticalPadding: CGFloat = CGFloat(AppSettings.candidateLineSpacing) / 2.0
 
-        private let textLine1: String = "傳統漢字\u{3000}\u{3000}\u{3000}"
-        private let textLine2: String = "傳統漢字・香港"
-        private let textLine3: String = "傳統漢字・臺灣"
-        private let textLine4: String = "简化字\u{3000}\u{3000}\u{3000}\u{3000}"
-
-        private struct LabelLines {
-                let textLine5: String
-                let textLine6: String
-                let textLine7: String
-                let textLine8: String
-                let textLine9: String
-                let textLine10: String
-        }
-        private let options: LabelLines = {
-                switch Logogram.current {
-                case .simplified:
-                        return LabelLines(textLine5: "半形数字", textLine6: "全形数字", textLine7: "粤文句读", textLine8: "英文标点", textLine9: "有 Emoji", textLine10: "无 Emoji")
-                default:
-                        return LabelLines(textLine5: "半形數字", textLine6: "全形數字", textLine7: "粵文句讀", textLine8: "英文標點", textLine9: "有 Emoji", textLine10: "無 Emoji")
-                }
+        private let options: [String] = {
+                let variants: [String] = [
+                        "傳統漢字\u{3000}\u{3000}\u{3000}",
+                        "傳統漢字・香港",
+                        "傳統漢字・臺灣",
+                        "简化字\u{3000}\u{3000}\u{3000}\u{3000}"
+                ]
+                let traditional: [String] = [
+                        "半形數字",
+                        "全形數字",
+                        "粵文句讀",
+                        "英文標點",
+                        "有 Emoji",
+                        "無 Emoji"
+                ]
+                let simplified: [String] = [
+                        "半形数字",
+                        "全形数字",
+                        "粤文句读",
+                        "英文标点",
+                        "有 Emoji",
+                        "无 Emoji"
+                ]
+                return (Logogram.current == .simplified) ? (variants + simplified) : (variants + traditional)
         }()
 
         private let currentVariant: Logogram = Logogram.current
@@ -53,25 +57,25 @@ struct InstantSettingsView: View {
                 let highlightedIndex = settingsObject.highlightedIndex
                 VStack(alignment: .leading, spacing: 0) {
                         Group {
-                                SettingLabel(verticalPadding: verticalPadding, index: 0, highlightedIndex: highlightedIndex, text: textLine1, checked: currentVariant == .traditional)
-                                SettingLabel(verticalPadding: verticalPadding, index: 1, highlightedIndex: highlightedIndex, text: textLine2, checked: currentVariant == .hongkong)
-                                SettingLabel(verticalPadding: verticalPadding, index: 2, highlightedIndex: highlightedIndex, text: textLine3, checked: currentVariant == .taiwan)
-                                SettingLabel(verticalPadding: verticalPadding, index: 3, highlightedIndex: highlightedIndex, text: textLine4, checked: currentVariant == .simplified)
+                                SettingLabel(verticalPadding: verticalPadding, index: 0, highlightedIndex: highlightedIndex, text: options[0], checked: currentVariant == .traditional)
+                                SettingLabel(verticalPadding: verticalPadding, index: 1, highlightedIndex: highlightedIndex, text: options[1], checked: currentVariant == .hongkong)
+                                SettingLabel(verticalPadding: verticalPadding, index: 2, highlightedIndex: highlightedIndex, text: options[2], checked: currentVariant == .taiwan)
+                                SettingLabel(verticalPadding: verticalPadding, index: 3, highlightedIndex: highlightedIndex, text: options[3], checked: currentVariant == .simplified)
                         }
                         Divider()
                         Group {
-                                SettingLabel(verticalPadding: verticalPadding, index: 4, highlightedIndex: highlightedIndex, text: options.textLine5, checked: InstantSettings.characterForm == .halfWidth)
-                                SettingLabel(verticalPadding: verticalPadding, index: 5, highlightedIndex: highlightedIndex, text: options.textLine6, checked: InstantSettings.characterForm == .fullWidth)
+                                SettingLabel(verticalPadding: verticalPadding, index: 4, highlightedIndex: highlightedIndex, text: options[4], checked: InstantSettings.characterForm == .halfWidth)
+                                SettingLabel(verticalPadding: verticalPadding, index: 5, highlightedIndex: highlightedIndex, text: options[5], checked: InstantSettings.characterForm == .fullWidth)
                         }
                         Divider()
                         Group {
-                                SettingLabel(verticalPadding: verticalPadding, index: 6, highlightedIndex: highlightedIndex, text: options.textLine7, checked: InstantSettings.punctuation == .cantonese)
-                                SettingLabel(verticalPadding: verticalPadding, index: 7, highlightedIndex: highlightedIndex, text: options.textLine8, checked: InstantSettings.punctuation == .english)
+                                SettingLabel(verticalPadding: verticalPadding, index: 6, highlightedIndex: highlightedIndex, text: options[6], checked: InstantSettings.punctuation == .cantonese)
+                                SettingLabel(verticalPadding: verticalPadding, index: 7, highlightedIndex: highlightedIndex, text: options[7], checked: InstantSettings.punctuation == .english)
                         }
                         Divider()
                         Group {
-                                SettingLabel(verticalPadding: verticalPadding, index: 8, highlightedIndex: highlightedIndex, text: options.textLine9, checked: InstantSettings.needsEmojiCandidates)
-                                SettingLabel(verticalPadding: verticalPadding, index: 9, highlightedIndex: highlightedIndex, text: options.textLine10, checked: !(InstantSettings.needsEmojiCandidates))
+                                SettingLabel(verticalPadding: verticalPadding, index: 8, highlightedIndex: highlightedIndex, text: options[8], checked: InstantSettings.needsEmojiCandidates)
+                                SettingLabel(verticalPadding: verticalPadding, index: 9, highlightedIndex: highlightedIndex, text: options[9], checked: !(InstantSettings.needsEmojiCandidates))
                         }
                 }
                 .padding(8)
