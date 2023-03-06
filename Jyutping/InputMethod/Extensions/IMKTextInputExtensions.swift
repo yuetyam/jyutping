362bd7c5e8aba612455b13ff90aac596ddd21569
Bing Zheung <bing@ososo.io>
2023-03-06 16:29:41 +0800

    (InputMethod) Fix IMKTextInput.insert(_text:)

--- Jyutping/InputMethod/Extensions/IMKTextInputExtensions.swift
+++ Jyutping/InputMethod/Extensions/IMKTextInputExtensions.swift
@@ -11,17 +11,20 @@ extension IMKTextInput {
         }
 
         func insert(_ text: String) {
-                clearMarkedText()
                 let convertedText: NSString = text as NSString
-                self.insertText(convertedText, replacementRange: NSRange(location: NSNotFound, length: NSNotFound))
+                self.insertText(convertedText, replacementRange: NSRange.replacementRange)
         }
 
         func mark(_ text: String) {
                 let convertedText: NSString = text as NSString
-                self.setMarkedText(convertedText, selectionRange: NSRange(location: convertedText.length, length: 0), replacementRange: NSRange(location: NSNotFound, length: 0))
+                self.setMarkedText(convertedText, selectionRange: NSRange(location: convertedText.length, length: 0), replacementRange: NSRange.replacementRange)
         }
 
         func clearMarkedText() {
-                self.setMarkedText(NSString(), selectionRange: NSRange(location: 0, length: 0), replacementRange: NSRange(location: NSNotFound, length: 0))
+                self.setMarkedText(NSString(), selectionRange: NSRange(location: 0, length: 0), replacementRange: NSRange.replacementRange)
         }
+}
+
+private extension NSRange {
+        static let replacementRange = NSRange(location: NSNotFound, length: 0)
 }
