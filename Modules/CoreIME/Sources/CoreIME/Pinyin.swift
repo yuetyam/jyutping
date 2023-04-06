7fb65bb754a1ac035ba0fcb69970b58ef764daf1
Bing Zheung <bing@ososo.io>
2023-04-06 11:03:19 +0800

    (CoreIME) Replace `CoreLexicon` with `PinyinLexicon`

--- Modules/CoreIME/Sources/CoreIME/Pinyin.swift
+++ Modules/CoreIME/Sources/CoreIME/Pinyin.swift
@@ -1,6 +1,13 @@
 import Foundation
 import SQLite3
 
+private struct PinyinLexicon: Hashable {
+        /// Lexicon text for the input
+        let text: String
+        /// Input text for the lexicon
+        let input: String
+}
+
 extension Engine {
 
         public static func pinyinReverseLookup(text: String, schemes: [[String]]) -> [Candidate] {
@@ -8,35 +15,35 @@ extension Engine {
                 return candidates.flatMap({ $0 })
         }
 
-        private static func search(pinyin text: String, schemes: [[String]]) -> [CoreLexicon] {
+        private static func search(pinyin text: String, schemes: [[String]]) -> [PinyinLexicon] {
                 let fullProcessed = match(text: text) + shortcut(text: text)
                 guard schemes.subelementCount > 0 else { return fullProcessed.uniqued() }
                 let matches = schemes.map({ match(text: $0.joined()) }).flatMap({ $0 })
                 return (fullProcessed + matches).uniqued()
         }
 
-        private static func match(text: String) -> [CoreLexicon] {
-                var items: [CoreLexicon] = []
+        private static func match(text: String) -> [PinyinLexicon] {
+                var items: [PinyinLexicon] = []
                 let query = "SELECT word FROM pinyintable WHERE pin = \(text.hash);"
                 var statement: OpaquePointer? = nil
                 defer { sqlite3_finalize(statement) }
                 guard sqlite3_prepare_v2(Engine.database, query, -1, &statement, nil) == SQLITE_OK else { return items }
                 while sqlite3_step(statement) == SQLITE_ROW {
                         let word: String = String(cString: sqlite3_column_text(statement, 0))
-                        let candidate = CoreLexicon(input: text, text: word)
+                        let candidate = PinyinLexicon(text: word, input: text)
                         items.append(candidate)
                 }
                 return items
         }
-        private static func shortcut(text: String) -> [CoreLexicon] {
-                var items: [CoreLexicon] = []
+        private static func shortcut(text: String) -> [PinyinLexicon] {
+                var items: [PinyinLexicon] = []
                 let query = "SELECT word FROM pinyintable WHERE shortcut = \(text.hash) LIMIT 100;"
                 var statement: OpaquePointer? = nil
                 defer { sqlite3_finalize(statement) }
                 guard sqlite3_prepare_v2(Engine.database, query, -1, &statement, nil) == SQLITE_OK else { return items }
                 while sqlite3_step(statement) == SQLITE_ROW {
                         let word: String = String(cString: sqlite3_column_text(statement, 0))
-                        let candidate = CoreLexicon(input: text, text: word)
+                        let candidate = PinyinLexicon(text: word, input: text)
                         items.append(candidate)
                 }
                 return items
