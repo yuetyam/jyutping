103acbc3f9a95349214759a1961386a80df772dc
Bing ZHEUNG <bing@zheung.org>
2023-10-06 22:03:41 +0800

    Fix a typo

--- Jyutping/InputMethod/Preferences/AboutView.swift
+++ Jyutping/InputMethod/Preferences/AboutView.swift
@@ -17,22 +17,22 @@ struct AboutView: View {
                                 .block()
 
                                 VStack {
-                                        LinkLabel(icon: "globe.asia.australia", tittle: "Website", link: "https://jyutping.app")
-                                        LinkLabel(icon: "chevron.left.forwardslash.chevron.right", tittle: "Source Code", link: "https://github.com/yuetyam/jyutping")
-                                        LinkLabel(icon: "lock.circle", tittle: "Privacy Policy", link: "https://jyutping.app/privacy")
+                                        LinkLabel(icon: "globe.asia.australia", title: "Website", link: "https://jyutping.app")
+                                        LinkLabel(icon: "chevron.left.forwardslash.chevron.right", title: "Source Code", link: "https://github.com/yuetyam/jyutping")
+                                        LinkLabel(icon: "lock.circle", title: "Privacy Policy", link: "https://jyutping.app/privacy")
                                 }
                                 .block()
 
                                 VStack {
-                                        LinkLabel(icon: "paperplane", tittle: "Telegram Group", link: "https://t.me/jyutping")
-                                        LinkLabel(icon: "person.2", tittle: "QQ Group", link: #"https://jq.qq.com/?k=4PR17m3t"#, message: "293148593")
+                                        LinkLabel(icon: "paperplane", title: "Telegram Group", link: "https://t.me/jyutping")
+                                        LinkLabel(icon: "person.2", title: "QQ Group", link: #"https://jq.qq.com/?k=4PR17m3t"#, message: "293148593")
                                 }
                                 .block()
 
                                 VStack {
-                                        // LinkLabel(icon: "t.square", tittle: "TRUTH Social", link: "https://truthsocial.com/@Cantonese")
-                                        LinkLabel(icon: "at", tittle: "Twitter", link: "https://twitter.com/JyutpingApp")
-                                        LinkLabel(icon: "circle.square", tittle: "Instagram", link: "https://www.instagram.com/jyutping_app")
+                                        // LinkLabel(icon: "t.square", title: "TRUTH Social", link: "https://truthsocial.com/@Cantonese")
+                                        LinkLabel(icon: "at", title: "Twitter", link: "https://twitter.com/JyutpingApp")
+                                        LinkLabel(icon: "circle.square", title: "Instagram", link: "https://www.instagram.com/jyutping_app")
                                 }
                                 .block()
                         }
@@ -53,15 +53,15 @@ struct AboutView: View {
 
 private struct LinkLabel: View {
 
-        init(icon: String, tittle: LocalizedStringKey, link: String, message: String? = nil) {
+        init(icon: String, title: LocalizedStringKey, link: String, message: String? = nil) {
                 self.icon = icon
-                self.tittle = tittle
+                self.title = title
                 self.link = link
                 self.message = message ?? link
         }
 
         private let icon: String
-        private let tittle: LocalizedStringKey
+        private let title: LocalizedStringKey
         private let link: String
         private let message: String
 
@@ -73,7 +73,7 @@ private struct LinkLabel: View {
                                                 .resizable()
                                                 .scaledToFit()
                                                 .frame(width: 16, height: 16)
-                                        Text(tittle)
+                                        Text(title)
                                 }
                         }
                         Text(verbatim: message).font(.callout.monospaced())
