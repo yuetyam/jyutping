6b57ec4e25f04a5aa4e0190012910f76b148d7de
Bing ZHEUNG <bing@zheung.org>
2023-12-30 02:03:39 +0800

    Improve packaging

--- packaging/jyutping.rb
+++ packaging/jyutping.rb
@@ -1,5 +1,5 @@
 cask "jyutping" do
-  version "0.24.0"
+  version "0.33.0"
   sha256 "627253043408789066a8af463f84cb445d18dc2cc36ab9400843ceeaf6b58353"
 
   url "https://github.com/yuetyam/jyutping/releases/download/#{version}/Jyutping-v#{version}-Mac-IME.pkg.zip",
@@ -8,16 +8,18 @@ cask "jyutping" do
   desc "Cantonese Jyutping Input Method"
   homepage "https://jyutping.app/"
 
+  auto_updates true
   depends_on macos: ">= :monterey"
 
   pkg "Jyutping-v#{version}-Mac-IME.pkg"
 
-  uninstall pkgutil: "org.jyutping.inputmethod.Jyutping",
+  uninstall quit:    "org.jyutping.inputmethod.Jyutping",
+            pkgutil: "org.jyutping.inputmethod.Jyutping",
             delete:  "/Library/Input Methods/Jyutping.app"
 
   zap trash: [
     "~/Library/Application Scripts/org.jyutping.inputmethod.Jyutping",
+    "~/Library/Caches/org.jyutping.inputmethod.Jyutping",
     "~/Library/Containers/org.jyutping.inputmethod.Jyutping",
-    "~/Library/Preferences/org.jyutping.inputmethod.Jyutping.plist",
   ]
 end
