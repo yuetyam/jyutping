069bd57b7e541c13b0fc50a0b882e54503615f5d
Bing ZHEUNG <bing@zheung.org>
2024-04-03 03:06:27 +0800

    (Jyutping App) Update Jyutping final table texts

--- Jyutping/Linguistics/FinalTable.swift
+++ Jyutping/Linguistics/FinalTable.swift
@@ -65,51 +65,51 @@ struct FinalTable: View {
 
 private let sourceText: String = """
 例字,國際音標,粵拼
-渣 zaa1,[ aː ],aa
-齋 zaai1,[ aːi ],aai
-嘲 zaau1,[ aːu ],aau
-站 zaam6,[ aːm ],aam
-讚 zaan3,[ aːn ],aan
-爭 zaang1,[ aːŋ ],aang
-雜 zaap6,[ aːp̚ ],aap
-扎 zaat3,[ aːt̚ ],aat
-責 zaak3,[ aːk̚ ],aak
+駕 gaa3,[ aː ],aa
+界 gaai3,[ aːi ],aai
+教 gaau3,[ aːu ],aau
+鑑 gaam3,[ aːm ],aam
+諫 gaan3,[ aːn ],aan
+耕 gaang1,[ aːŋ ],aang
+甲 gaap3,[ aːp̚ ],aap
+戛 gaat3,[ aːt̚ ],aat
+格 gaak3,[ aːk̚ ],aak
 .
 嘞 la3,[ ɐ ],a
-擠 zai1,[ ɐi ],ai
-周 zau1,[ ɐu ],au
-針 zam1,[ ɐm ],am
-真 zan1,[ ɐn ],an
-增 zang1,[ ɐŋ ],ang
-汁 zap1,[ ɐp̚ ],ap
-質 zat1,[ ɐt̚ ],at
-則 zak1,[ ɐk̚ ],ak
+計 gai3,[ ɐi ],ai
+救 gau3,[ ɐu ],au
+禁 gam3,[ ɐm ],am
+斤 gan1,[ ɐn ],an
+庚 gang1,[ ɐŋ ],ang
+急 gap1,[ ɐp̚ ],ap
+吉 gat1,[ ɐt̚ ],at
+北 bak1,[ ɐk̚ ],ak
 .
-遮 ze1,[ ɛː ],e
-悲 bei1,[ ei ],ei
+嘅 ge3,[ ɛː ],e
+記 gei3,[ ei ],ei
 掉 deu6,[ ɛːu ],eu
 𦧷 lem2,[ ɛːm ],em
-鄭 zeng6,[ ɛːŋ ],eng
+鏡 geng3,[ ɛːŋ ],eng
 夾 gep6,[ ɛːp̚ ],ep
 坺 pet6,[ ɛːt̚ ],et
-隻 zek3,[ ɛːk̚ ],ek
+踢 tek3,[ ɛːk̚ ],ek
 .
-之 zi1,[ iː ],i
-招 ziu1,[ iːu ],iu
-尖 zim1,[ iːm ],im
-煎 zin1,[ iːn ],in
-晶 zing1,[ eŋ ],ing
-接 zip3,[ iːp̚ ],ip
-節 zit3,[ iːt̚ ],it
-即 zik1,[ ek̚ ],ik
+意 ji3,[ iː ],i
+叫 giu3,[ iːu ],iu
+劍 gim3,[ iːm ],im
+見 gin3,[ iːn ],in
+敬 ging3,[ eŋ ],ing
+劫 gip3,[ iːp̚ ],ip
+結 git3,[ iːt̚ ],it
+極 gik6,[ ek̚ ],ik
 .
-左 zo2,[ ɔː ],o
-栽 zoi1,[ ɔːi ],oi
-租 zou1,[ ou ],ou
-肝 gon1,[ ɔːn ],on
-裝 zong1,[ ɔːŋ ],ong
-喝 hot3,[ ɔːt̚ ],ot
-作 zok3,[ ɔːk̚ ],ok
+個 go3,[ ɔː ],o
+菜 coi3,[ ɔːi ],oi
+告 gou3,[ ou ],ou
+幹 gon3,[ ɔːn ],on
+鋼 gong3,[ ɔːŋ ],ong
+割 got3,[ ɔːt̚ ],ot
+各 gok3,[ ɔːk̚ ],ok
 .
 夫 fu1,[ uː ],u
 灰 fui1,[ uːi ],ui
@@ -118,16 +118,16 @@ private let sourceText: String = """
 闊 fut3,[ uːt̚ ],ut
 福 fuk1,[ ok̚ ],uk
 .
-靴 hoe1,[ œː ],oe
-香 hoeng1,[ œːŋ ],oeng
+鋸 goe3,[ œː ],oe
+姜 goeng1,[ œːŋ ],oeng
 腳 goek3,[ œːk̚ ],oek
 .
-追 zeoi1,[ ɵy ],eoi
-津 zeon1,[ ɵn ],eon
-卒 zeot1,[ ɵt̚ ],eot
+歲 seoi3,[ ɵy ],eoi
+信 seon3,[ ɵn ],eon
+術 seot6,[ ɵt̚ ],eot
 .
-書 syu1,[ yː ],yu
-酸 syun1,[ yːn ],yun
+恕 syu3,[ yː ],yu
+算 syun3,[ yːn ],yun
 雪 syut3,[ yːt̚ ],yut
 """
 
