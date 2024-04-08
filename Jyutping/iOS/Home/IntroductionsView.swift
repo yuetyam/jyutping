3109286618c801da33c9dffa954585dff4bc20f1
Bing ZHEUNG <bing@zheung.org>
2024-04-08 12:31:32 +0800

    (Jyutping App) Add SaamPing keyboard layout introductions

--- Jyutping/iOS/Home/IntroductionsView.swift
+++ Jyutping/iOS/Home/IntroductionsView.swift
@@ -13,6 +13,11 @@ struct IntroductionsView: View {
                                 Text("IOSHomeTab.IntroductionsView.Heading.ClearPreEdit").font(.significant)
                                 Text("IOSHomeTab.IntroductionsView.Body.ClearPreEdit").lineSpacing(6)
                         }
+                        Section {
+                                Text("IOSHomeTab.IntroductionsView.Heading.SaamPingKeyboard").font(.significant)
+                                Text("IOSHomeTab.IntroductionsView.Body.SaamPingKeyboard.Row1").lineSpacing(6)
+                                Text("IOSHomeTab.IntroductionsView.Body.SaamPingKeyboard.Row2").lineSpacing(6)
+                        }
                 }
                 .textSelection(.enabled)
                 .navigationTitle("IOSHomeTab.NavigationTitle.MoreIntroductions")
