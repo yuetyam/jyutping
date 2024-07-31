9a356e66b901b3681ae3d733a3976bf7f3f8b25e
Bing ZHEUNG <bing@zheung.org>
2024-07-31 17:53:06 +0800

    (InputMethod) Improve ReverseLookupView

--- InputMethod/Preferences/ReverseLookupView.swift
+++ InputMethod/Preferences/ReverseLookupView.swift
@@ -8,19 +8,72 @@ struct ReverseLookupView: View {
         var body: some View {
                 ScrollView {
                         LazyVStack(spacing: 16) {
-                                HStack {
-                                        Picker("ReverseLookupView.CangjieVariant.Picker.TitleKey", selection: $cangjieVariant) {
-                                                Text("ReverseLookupView.CangjieVariant.Picker.Option1").tag(CangjieVariant.cangjie5)
-                                                Text("ReverseLookupView.CangjieVariant.Picker.Option2").tag(CangjieVariant.cangjie3)
-                                                Text("ReverseLookupView.CangjieVariant.Picker.Option3").tag(CangjieVariant.quick5)
-                                                Text("ReverseLookupView.CangjieVariant.Picker.Option4").tag(CangjieVariant.quick3)
+                                VStack {
+                                        HStack {
+                                                Text("ReverseLookupView.PinyinReverseLookup.Heading").font(.headline)
+                                                Spacer()
                                         }
-                                        .pickerStyle(.menu)
-                                        .scaledToFit()
-                                        .onChange(of: cangjieVariant) { newVariant in
-                                                AppSettings.updateCangjieVariant(to: newVariant)
+                                        Divider()
+                                        HStack {
+                                                Text("ReverseLookupView.PinyinReverseLookup.Body")
+                                                Spacer()
                                         }
-                                        Spacer()
+                                }
+                                .block()
+                                VStack {
+                                        HStack {
+                                                Text("ReverseLookupView.CangjieReverseLookup.Heading").font(.headline)
+                                                Spacer()
+                                        }
+                                        Divider()
+                                        HStack {
+                                                Text("ReverseLookupView.CangjieReverseLookup.Body")
+                                                Spacer()
+                                        }
+                                        Divider()
+                                        HStack {
+                                                Picker("ReverseLookupView.CangjieVariant.Picker.TitleKey", selection: $cangjieVariant) {
+                                                        Text("ReverseLookupView.CangjieVariant.Picker.Option1").tag(CangjieVariant.cangjie5)
+                                                        Text("ReverseLookupView.CangjieVariant.Picker.Option2").tag(CangjieVariant.cangjie3)
+                                                        Text("ReverseLookupView.CangjieVariant.Picker.Option3").tag(CangjieVariant.quick5)
+                                                        Text("ReverseLookupView.CangjieVariant.Picker.Option4").tag(CangjieVariant.quick3)
+                                                }
+                                                .pickerStyle(.menu)
+                                                .scaledToFit()
+                                                .onChange(of: cangjieVariant) { newVariant in
+                                                        AppSettings.updateCangjieVariant(to: newVariant)
+                                                }
+                                                Spacer()
+                                        }
+                                }
+                                .block()
+                                VStack {
+                                        HStack {
+                                                Text("ReverseLookupView.StrokeReverseLookup.Heading").font(.headline)
+                                                Spacer()
+                                        }
+                                        Divider()
+                                        HStack {
+                                                Text("ReverseLookupView.StrokeReverseLookup.Body")
+                                                Spacer()
+                                        }
+                                        Divider()
+                                        HStack {
+                                                Text("ReverseLookupView.StrokeReverseLookup.Example").font(.body.monospaced())
+                                                Spacer()
+                                        }
+                                }
+                                .block()
+                                VStack {
+                                        HStack {
+                                                Text("ReverseLookupView.StructureReverseLookup.Heading").font(.headline)
+                                                Spacer()
+                                        }
+                                        Divider()
+                                        HStack {
+                                                Text("ReverseLookupView.StructureReverseLookup.Body")
+                                                Spacer()
+                                        }
                                 }
                                 .block()
                         }
