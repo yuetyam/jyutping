365e7e42ad9907782522d82852982267a7d4d56c
Bing ZHEUNG <bing@zheung.org>
2025-07-09 20:29:18 +0800

    (InputMethod) Improve UserLexicon suggest()

--- InputMethod/Extensions/StringExtensions.swift
+++ InputMethod/Extensions/StringExtensions.swift
@@ -3,49 +3,31 @@ import CommonExtensions
 
 extension String {
 
-        var ping: Int {
-                return self.removedSpacesTones().hash
+        var pingCode: Int {
+                return filter(\.isLowercaseBasicLatinLetter).hash
         }
 
-        var shortcut: Int {
-                let anchors = self.split(separator: Character.space).compactMap(\.first)
+        var shortcutCode: Int {
+                let anchors = split(separator: Character.space).compactMap(\.first)
                 return String(anchors).hash
         }
 
         /// A subsequence that only contains tones (1-6)
         var tones: String {
-                return self.filter(\.isCantoneseToneDigit)
+                return filter(\.isCantoneseToneDigit)
         }
 
         /// Remove all tones (1-6)
         /// - Returns: A subsequence that leaves off the tones.
         func removedTones() -> String {
-                return self.filter({ !$0.isCantoneseToneDigit })
-        }
-
-        /// Remove all spaces and tones
-        /// - Returns: A subsequence that leaves off the spaces and tones.
-        func removedSpacesTones() -> String {
-                return self.filter({ !$0.isSpaceOrTone })
-        }
-
-        /// Remove all spaces, separators and tones
-        /// - Returns: A subsequence that leaves off the spaces, separators and tones.
-        func removedSpacesSeparatorsTones() -> String {
-                return self.filter({ $0.isSpaceOrSeparatorOrTone })
-        }
-
-        /// check if the first character is letter
-        var isLetters: Bool {
-                return self.first?.isBasicLatinLetter ?? false
+                return filter(\.isCantoneseToneDigit.negative)
         }
 }
 
-
 extension StringProtocol {
 
-        /// Transform to Full-width characters
-        /// - Returns: A new String with Full-width characters
+        /// Transform to full-width characters
+        /// - Returns: A new String with full-width characters
         func fullWidth() -> String {
                 return self.applyingTransform(.fullwidthToHalfwidth, reverse: true) ?? (self as? String) ?? String(self)
         }
