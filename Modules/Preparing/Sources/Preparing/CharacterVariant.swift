8b38d1b8d0c82a464b7ccd19210a2e610db2ae99
Bing ZHEUNG <bing@zheung.org>
2025-12-17 11:39:59 +0800

    (Preparing) Use GenericCJKVCodePoint checks in CharacterVariant

--- Modules/Preparing/Sources/Preparing/CharacterVariant.swift
+++ Modules/Preparing/Sources/Preparing/CharacterVariant.swift
@@ -11,13 +11,13 @@ struct CharacterVariant {
                 let sourceLines: [String] = sourceContent
                         .trimmingCharacters(in: .whitespacesAndNewlines)
                         .components(separatedBy: .newlines)
-                        .filter({ !$0.isEmpty })
+                        .filter(\.isNotEmpty)
                         .map({ $0.trimmingCharacters(in: .whitespaces).trimmingCharacters(in: .controlCharacters) })
                         .filter({ !($0.isEmpty || $0.hasPrefix("#")) })
                         .distinct()
                 let variantMaps = sourceLines.flatMap { line -> [VariantMap] in
                         let errorMessage: String = "Bad format in line text: \(line)"
-                        // let warningMessage: String = "Not Ideographic: \(line)"
+                        let warningMessage: String = "Not Generic Ideographic: \(line)"
                         let parts = line.split(separator: "\t").map({ $0.trimmingCharacters(in: .whitespaces).trimmingCharacters(in: .controlCharacters) })
                         guard parts.count >= 2 else { fatalError(errorMessage) }
                         let leftText = parts[0]
@@ -30,12 +30,12 @@ struct CharacterVariant {
                                 guard let leftChar = leftText.first, let rightChar = rightText.first else { fatalError(errorMessage) }
                                 guard let leftCode = leftChar.unicodeScalars.first?.value else { fatalError(errorMessage) }
                                 guard let rightCode = rightChar.unicodeScalars.first?.value else { fatalError(errorMessage) }
-                                guard leftCode.isIdeographicCodePoint else {
-                                        // print(warningMessage)
+                                guard leftCode.isGenericCJKVCodePoint else {
+                                        print(warningMessage)
                                         return []
                                 }
-                                guard rightCode.isIdeographicCodePoint else {
-                                        // print(warningMessage)
+                                guard rightCode.isGenericCJKVCodePoint else {
+                                        print(warningMessage)
                                         return []
                                 }
                                 guard rightCode != leftCode else { fatalError(errorMessage) }
@@ -43,18 +43,17 @@ struct CharacterVariant {
                         default:
                                 guard let leftChar = leftText.first else { fatalError(errorMessage) }
                                 guard let leftCode = leftChar.unicodeScalars.first?.value else { fatalError(errorMessage) }
-                                guard leftCode.isIdeographicCodePoint else {
-                                        // print(warningMessage)
+                                guard leftCode.isGenericCJKVCodePoint else {
+                                        print(warningMessage)
                                         return []
                                 }
                                 let rightComponents = rightText.split(separator: String.space).map({ $0.trimmingCharacters(in: .whitespaces).trimmingCharacters(in: .controlCharacters) })
                                 let maps = rightComponents.compactMap({ text -> VariantMap? in
                                         guard let rightCode = text.first?.unicodeScalars.first?.value else { fatalError(errorMessage) }
-                                        guard rightCode.isIdeographicCodePoint else {
-                                                // print(warningMessage)
+                                        guard rightCode.isGenericCJKVCodePoint else {
+                                                print(warningMessage)
                                                 return nil
                                         }
-                                        // guard rightCode != leftCode else { fatalError(errorMessage) }
                                         return VariantMap(left: leftCode, right: rightCode)
                                 })
                                 return maps
