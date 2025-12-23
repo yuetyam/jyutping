d83b19985d0a61e6038c4a3e1a989ffafeca1e75
Bing ZHEUNG <bing@zheung.org>
2025-12-24 01:33:55 +0800

    (CoreIME) Improve Pinyin suggestions

--- Modules/CoreIME/Sources/CoreIME/Pinyin.swift
+++ Modules/CoreIME/Sources/CoreIME/Pinyin.swift
@@ -8,34 +8,34 @@ extension Engine {
                 // TODO: Handle separators
                 guard isLetterEventOnly else { return [] }
                 lazy var anchorsStatement: OpaquePointer? = preparePinyinAnchorsStatement()
-                lazy var pingStatement: OpaquePointer? = preparePinyinStatement()
+                lazy var spellStatement: OpaquePointer? = preparePinyinStatement()
                 defer {
                         sqlite3_finalize(anchorsStatement)
-                        sqlite3_finalize(pingStatement)
+                        sqlite3_finalize(spellStatement)
                 }
                 let canSegment: Bool = segmentation.flattenedCount > 0
                 if canSegment {
-                        return pinyinSearch(events: events, segmentation: segmentation, anchorsStatement: anchorsStatement, pingStatement: pingStatement)
+                        return pinyinSearch(events: events, segmentation: segmentation, anchorsStatement: anchorsStatement, spellStatement: spellStatement)
                                 .flatMap({ Engine.reveresLookup(text: $0.text, input: $0.input, mark: $0.mark) })
                 } else {
-                        return processPinyinSlices(of: events, text: events.map(\.text).joined(), anchorsStatement: anchorsStatement, pingStatement: pingStatement)
+                        return processPinyinSlices(of: events, text: events.map(\.text).joined(), anchorsStatement: anchorsStatement, spellStatement: spellStatement)
                                 .flatMap({ Engine.reveresLookup(text: $0.text, input: $0.input, mark: $0.mark) })
                 }
         }
 
-        private static func processPinyinSlices<T: RandomAccessCollection<InputEvent>>(of events: T, text: String, limit: Int64? = nil, anchorsStatement: OpaquePointer?, pingStatement: OpaquePointer?) -> [PinyinLexicon] {
+        private static func processPinyinSlices<T: RandomAccessCollection<InputEvent>>(of events: T, text: String, limit: Int64? = nil, anchorsStatement: OpaquePointer?, spellStatement: OpaquePointer?) -> [PinyinLexicon] {
                 let adjustedLimit: Int64 = (limit == nil) ? 300 : 100
                 let inputLength: Int = events.count
                 return (0..<inputLength).flatMap({ number -> [PinyinLexicon] in
                         let leadingEvents = events.dropLast(number)
                         let leadingText = leadingEvents.map(\.text).joined()
-                        let pingMatched = pinyinMatch(text: leadingText, limit: limit, statement: pingStatement)
+                        let spellMatched = pinyinSpellMatch(text: leadingText, limit: limit, statement: spellStatement)
                                 .map({ modify($0, text: text, textLength: inputLength) })
                         let anchorsMatched = pinyinAnchorsMatch(events: leadingEvents, input: leadingText, limit: adjustedLimit, statement: anchorsStatement)
                                 .map({ modify($0, text: text, textLength: inputLength) })
                                 .sorted()
                                 .prefix(72)
-                        return pingMatched + anchorsMatched
+                        return spellMatched + anchorsMatched
                 })
                 .distinct()
                 .sorted()
@@ -52,14 +52,14 @@ extension Engine {
                 return PinyinLexicon(text: item.text, pinyin: item.pinyin, input: text, mark: text, order: item.order)
         }
 
-        private static func pinyinSearch<T: RandomAccessCollection<InputEvent>>(events: T, segmentation: PinyinSegmentation, limit: Int64? = nil, anchorsStatement: OpaquePointer?, pingStatement: OpaquePointer?) -> [PinyinLexicon] {
+        private static func pinyinSearch<T: RandomAccessCollection<InputEvent>>(events: T, segmentation: PinyinSegmentation, limit: Int64? = nil, anchorsStatement: OpaquePointer?, spellStatement: OpaquePointer?) -> [PinyinLexicon] {
                 let inputLength: Int = events.count
                 let text: String = events.map(\.text).joined()
-                let pingMatched = pinyinMatch(text: text, limit: limit, statement: pingStatement)
+                let spellMatched = pinyinSpellMatch(text: text, limit: limit, statement: spellStatement)
                 let anchorsMatched = pinyinAnchorsMatch(events: events, limit: limit, statement: anchorsStatement)
-                let queried = pinyinQuery(inputLength: inputLength, segmentation: segmentation, limit: limit, statement: pingStatement)
+                let queried = pinyinQuery(inputLength: inputLength, segmentation: segmentation, limit: limit, statement: spellStatement)
                 let shouldMatchPrefixes: Bool = {
-                        guard pingMatched.isEmpty else { return false }
+                        guard spellMatched.isEmpty else { return false }
                         guard queried.contains(where: { $0.inputCount == inputLength }).negative else { return false }
                         return segmentation.contains(where: { $0.length == inputLength }).negative
                 }()
@@ -103,7 +103,7 @@ extension Engine {
                 let fetched: [PinyinLexicon] = {
                         let idealQueried = queried.filter({ $0.inputCount == inputLength }).sorted(by: { $0.order < $1.order }).distinct()
                         let notIdealQueried = queried.filter({ $0.inputCount < inputLength }).sorted().distinct()
-                        let fullInput = (pingMatched + idealQueried + anchorsMatched + prefixMatched + gainedMatched).distinct()
+                        let fullInput = (spellMatched + idealQueried + anchorsMatched + prefixMatched + gainedMatched).distinct()
                         let primary = fullInput.prefix(10)
                         let secondary = fullInput.sorted().prefix(10)
                         let tertiary = notIdealQueried.prefix(10)
@@ -111,14 +111,14 @@ extension Engine {
                         return (primary + secondary + tertiary + quaternary + fullInput + notIdealQueried).distinct()
                 }()
                 guard let firstInputCount = fetched.first?.inputCount else {
-                        return processPinyinSlices(of: events, text: text, limit: limit, anchorsStatement: anchorsStatement, pingStatement: pingStatement)
+                        return processPinyinSlices(of: events, text: text, limit: limit, anchorsStatement: anchorsStatement, spellStatement: spellStatement)
                 }
                 guard firstInputCount < inputLength else { return fetched }
                 let headInputLengths = fetched.map(\.inputCount).distinct()
                 let concatenated = headInputLengths.compactMap({ headLength -> PinyinLexicon? in
                         let tailEvents = events.dropFirst(headLength)
                         let tailSegmentation = PinyinSegmenter.segment(events: tailEvents)
-                        guard let tailLexicon = pinyinSearch(events: tailEvents, segmentation: tailSegmentation, limit: 50, anchorsStatement: anchorsStatement, pingStatement: pingStatement).first else { return nil }
+                        guard let tailLexicon = pinyinSearch(events: tailEvents, segmentation: tailSegmentation, limit: 50, anchorsStatement: anchorsStatement, spellStatement: spellStatement).first else { return nil }
                         guard let headLexicon = fetched.first(where: { $0.inputCount == headLength }) else { return nil }
                         return headLexicon + tailLexicon
                 }).distinct().sorted().prefix(1)
@@ -129,7 +129,7 @@ extension Engine {
                 let idealSchemes = segmentation.filter({ $0.length == inputLength })
                 if idealSchemes.isEmpty {
                         return segmentation.flatMap({ scheme -> [PinyinLexicon] in
-                                return pinyinMatch(text: scheme.map(\.text).joined(), limit: limit, statement: statement)
+                                return pinyinSpellMatch(text: scheme.map(\.text).joined(), limit: limit, statement: statement)
                         })
                 } else {
                         return idealSchemes.flatMap({ scheme -> [PinyinLexicon] in
@@ -137,10 +137,10 @@ extension Engine {
                                 case 0:
                                         return []
                                 case 1:
-                                        return pinyinMatch(text: scheme.map(\.text).joined(), limit: limit, statement: statement)
+                                        return pinyinSpellMatch(text: scheme.map(\.text).joined(), limit: limit, statement: statement)
                                 default:
                                         return (1...scheme.count).reversed().map({ scheme.prefix($0) }).flatMap({ slice -> [PinyinLexicon] in
-                                                return pinyinMatch(text: slice.map(\.text).joined(), limit: limit, statement: statement)
+                                                return pinyinSpellMatch(text: slice.map(\.text).joined(), limit: limit, statement: statement)
                                         })
                                 }
                         })
@@ -153,21 +153,20 @@ extension Engine {
                 guard sqlite3_prepare_v2(Engine.database, command, -1, &statement, nil) == SQLITE_OK else { return nil }
                 return statement
         }
-        private static func pinyinMatch(text: String, limit: Int64? = nil, statement: OpaquePointer?) -> [PinyinLexicon] {
-                let limit: Int64 = limit ?? -1
+        private static func pinyinSpellMatch(text: String, limit: Int64? = nil, statement: OpaquePointer?) -> [PinyinLexicon] {
                 sqlite3_reset(statement)
                 sqlite3_bind_int64(statement, 1, Int64(text.hashCode()))
-                sqlite3_bind_int64(statement, 2, limit)
-                var items: [PinyinLexicon] = []
+                sqlite3_bind_int64(statement, 2, (limit ?? -1))
+                var instances: [PinyinLexicon] = []
                 while sqlite3_step(statement) == SQLITE_ROW {
                         let rowID = sqlite3_column_int64(statement, 0)
                         guard let word = sqlite3_column_text(statement, 1) else { continue }
                         guard let romanization = sqlite3_column_text(statement, 2) else { continue }
                         let pinyin = String(cString: romanization)
                         let instance = PinyinLexicon(text: String(cString: word), pinyin: pinyin, input: text, mark: pinyin, order: Int(rowID))
-                        items.append(instance)
+                        instances.append(instance)
                 }
-                return items
+                return instances
         }
         private static func preparePinyinAnchorsStatement() -> OpaquePointer? {
                 let command: String = "SELECT rowid, word, romanization FROM pinyin_lexicon WHERE anchors = ? LIMIT ?;"
@@ -181,16 +180,16 @@ extension Engine {
                 sqlite3_reset(statement)
                 sqlite3_bind_int64(statement, 1, Int64(code))
                 sqlite3_bind_int64(statement, 2, (limit ?? 100))
-                let text: String = input ?? events.map(\.text).joined()
-                var items: [PinyinLexicon] = []
+                let inputText: String = input ?? events.map(\.text).joined()
+                var instances: [PinyinLexicon] = []
                 while sqlite3_step(statement) == SQLITE_ROW {
                         let rowID = sqlite3_column_int64(statement, 0)
                         guard let word = sqlite3_column_text(statement, 1) else { continue }
                         guard let romanization = sqlite3_column_text(statement, 2) else { continue }
-                        let instance = PinyinLexicon(text: String(cString: word), pinyin: String(cString: romanization), input: text, mark: text, order: Int(rowID))
-                        items.append(instance)
+                        let instance = PinyinLexicon(text: String(cString: word), pinyin: String(cString: romanization), input: inputText, mark: inputText, order: Int(rowID))
+                        instances.append(instance)
                 }
-                return items
+                return instances
         }
 }
 
