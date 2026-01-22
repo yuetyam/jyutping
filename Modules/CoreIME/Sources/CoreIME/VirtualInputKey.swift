f2e22a0da5f56a9838a9d997b38bcd2cef556452
Bing ZHEUNG <bing@zheung.org>
2026-01-22 08:44:55 +0800

    (CoreIME) Add character to VirtualInputKey

--- Modules/CoreIME/Sources/CoreIME/VirtualInputKey.swift
+++ Modules/CoreIME/Sources/CoreIME/VirtualInputKey.swift
@@ -1,6 +1,9 @@
 public struct VirtualInputKey: Hashable, Identifiable, Comparable, Sendable {
 
-        /// Key text
+        /// Key character
+        public let character: Character
+
+        /// Key character as String
         public let text: String
 
         /// Unique identifier code
@@ -12,8 +15,9 @@ public struct VirtualInputKey: Hashable, Identifiable, Comparable, Sendable {
         /// Identifiable. Same value as the `code`
         public let id: Int
 
-        private init(text: String, code: Int, keyCode: UInt16) {
-                self.text = text
+        private init(_ character: Character, code: Int, keyCode: UInt16) {
+                self.character = character
+                self.text = String(character)
                 self.code = code
                 self.keyCode = keyCode
                 self.id = code
@@ -34,49 +38,49 @@ public struct VirtualInputKey: Hashable, Identifiable, Comparable, Sendable {
                 return lhs.code < rhs.code
         }
 
-        public static let number0 = VirtualInputKey(text: "0", code: 10, keyCode: 0x1D)
-        public static let number1 = VirtualInputKey(text: "1", code: 11, keyCode: 0x12)
-        public static let number2 = VirtualInputKey(text: "2", code: 12, keyCode: 0x13)
-        public static let number3 = VirtualInputKey(text: "3", code: 13, keyCode: 0x14)
-        public static let number4 = VirtualInputKey(text: "4", code: 14, keyCode: 0x15)
-        public static let number5 = VirtualInputKey(text: "5", code: 15, keyCode: 0x17)
-        public static let number6 = VirtualInputKey(text: "6", code: 16, keyCode: 0x16)
-        public static let number7 = VirtualInputKey(text: "7", code: 17, keyCode: 0x1A)
-        public static let number8 = VirtualInputKey(text: "8", code: 18, keyCode: 0x1C)
-        public static let number9 = VirtualInputKey(text: "9", code: 19, keyCode: 0x19)
+        public static let number0 = VirtualInputKey("0", code: 10, keyCode: 0x1D)
+        public static let number1 = VirtualInputKey("1", code: 11, keyCode: 0x12)
+        public static let number2 = VirtualInputKey("2", code: 12, keyCode: 0x13)
+        public static let number3 = VirtualInputKey("3", code: 13, keyCode: 0x14)
+        public static let number4 = VirtualInputKey("4", code: 14, keyCode: 0x15)
+        public static let number5 = VirtualInputKey("5", code: 15, keyCode: 0x17)
+        public static let number6 = VirtualInputKey("6", code: 16, keyCode: 0x16)
+        public static let number7 = VirtualInputKey("7", code: 17, keyCode: 0x1A)
+        public static let number8 = VirtualInputKey("8", code: 18, keyCode: 0x1C)
+        public static let number9 = VirtualInputKey("9", code: 19, keyCode: 0x19)
 
-        public static let letterA = VirtualInputKey(text: "a", code: 20, keyCode: 0x00)
-        public static let letterB = VirtualInputKey(text: "b", code: 21, keyCode: 0x0B)
-        public static let letterC = VirtualInputKey(text: "c", code: 22, keyCode: 0x08)
-        public static let letterD = VirtualInputKey(text: "d", code: 23, keyCode: 0x02)
-        public static let letterE = VirtualInputKey(text: "e", code: 24, keyCode: 0x0E)
-        public static let letterF = VirtualInputKey(text: "f", code: 25, keyCode: 0x03)
-        public static let letterG = VirtualInputKey(text: "g", code: 26, keyCode: 0x05)
-        public static let letterH = VirtualInputKey(text: "h", code: 27, keyCode: 0x04)
-        public static let letterI = VirtualInputKey(text: "i", code: 28, keyCode: 0x22)
-        public static let letterJ = VirtualInputKey(text: "j", code: 29, keyCode: 0x26)
-        public static let letterK = VirtualInputKey(text: "k", code: 30, keyCode: 0x28)
-        public static let letterL = VirtualInputKey(text: "l", code: 31, keyCode: 0x25)
-        public static let letterM = VirtualInputKey(text: "m", code: 32, keyCode: 0x2E)
-        public static let letterN = VirtualInputKey(text: "n", code: 33, keyCode: 0x2D)
-        public static let letterO = VirtualInputKey(text: "o", code: 34, keyCode: 0x1F)
-        public static let letterP = VirtualInputKey(text: "p", code: 35, keyCode: 0x23)
-        public static let letterQ = VirtualInputKey(text: "q", code: 36, keyCode: 0x0C)
-        public static let letterR = VirtualInputKey(text: "r", code: 37, keyCode: 0x0F)
-        public static let letterS = VirtualInputKey(text: "s", code: 38, keyCode: 0x01)
-        public static let letterT = VirtualInputKey(text: "t", code: 39, keyCode: 0x11)
-        public static let letterU = VirtualInputKey(text: "u", code: 40, keyCode: 0x20)
-        public static let letterV = VirtualInputKey(text: "v", code: 41, keyCode: 0x09)
-        public static let letterW = VirtualInputKey(text: "w", code: 42, keyCode: 0x0D)
-        public static let letterX = VirtualInputKey(text: "x", code: 43, keyCode: 0x07)
-        public static let letterY = VirtualInputKey(text: "y", code: 44, keyCode: 0x10)
-        public static let letterZ = VirtualInputKey(text: "z", code: 45, keyCode: 0x06)
+        public static let letterA = VirtualInputKey("a", code: 20, keyCode: 0x00)
+        public static let letterB = VirtualInputKey("b", code: 21, keyCode: 0x0B)
+        public static let letterC = VirtualInputKey("c", code: 22, keyCode: 0x08)
+        public static let letterD = VirtualInputKey("d", code: 23, keyCode: 0x02)
+        public static let letterE = VirtualInputKey("e", code: 24, keyCode: 0x0E)
+        public static let letterF = VirtualInputKey("f", code: 25, keyCode: 0x03)
+        public static let letterG = VirtualInputKey("g", code: 26, keyCode: 0x05)
+        public static let letterH = VirtualInputKey("h", code: 27, keyCode: 0x04)
+        public static let letterI = VirtualInputKey("i", code: 28, keyCode: 0x22)
+        public static let letterJ = VirtualInputKey("j", code: 29, keyCode: 0x26)
+        public static let letterK = VirtualInputKey("k", code: 30, keyCode: 0x28)
+        public static let letterL = VirtualInputKey("l", code: 31, keyCode: 0x25)
+        public static let letterM = VirtualInputKey("m", code: 32, keyCode: 0x2E)
+        public static let letterN = VirtualInputKey("n", code: 33, keyCode: 0x2D)
+        public static let letterO = VirtualInputKey("o", code: 34, keyCode: 0x1F)
+        public static let letterP = VirtualInputKey("p", code: 35, keyCode: 0x23)
+        public static let letterQ = VirtualInputKey("q", code: 36, keyCode: 0x0C)
+        public static let letterR = VirtualInputKey("r", code: 37, keyCode: 0x0F)
+        public static let letterS = VirtualInputKey("s", code: 38, keyCode: 0x01)
+        public static let letterT = VirtualInputKey("t", code: 39, keyCode: 0x11)
+        public static let letterU = VirtualInputKey("u", code: 40, keyCode: 0x20)
+        public static let letterV = VirtualInputKey("v", code: 41, keyCode: 0x09)
+        public static let letterW = VirtualInputKey("w", code: 42, keyCode: 0x0D)
+        public static let letterX = VirtualInputKey("x", code: 43, keyCode: 0x07)
+        public static let letterY = VirtualInputKey("y", code: 44, keyCode: 0x10)
+        public static let letterZ = VirtualInputKey("z", code: 45, keyCode: 0x06)
 
         /// Separator; Delimiter; Quote
-        public static let apostrophe = VirtualInputKey(text: "\u{27}", code: 47, keyCode: 0x27)
+        public static let apostrophe = VirtualInputKey("\u{27}", code: 47, keyCode: 0x27)
 
         /// Grave accent; Backtick; Backquote
-        public static let grave = VirtualInputKey(text: "\u{60}", code: 48, keyCode: 0x32)
+        public static let grave = VirtualInputKey("\u{60}", code: 48, keyCode: 0x32)
 }
 
 extension VirtualInputKey {
