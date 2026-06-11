b3be07c76bb0818f59a6a980ef86f1a1233e2784
Bing ZHEUNG <bing@zheung.org>
2026-06-11 17:16:55 +0800

    Fix CI script paths

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,4 +3,6 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_ROOT="$(dirname "$SCRIPT_DIR")"
 
-swift run --package-path "$REPO_ROOT/Modules/Preparing" -c release
+cd "$REPO_ROOT/Modules/Preparing"
+
+swift run -c release
