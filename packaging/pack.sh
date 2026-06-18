559ec4ae1e0751c68be466e33fd19c10f9ab3352
Bing ZHEUNG <bing@zheung.org>
2026-06-18 16:55:35 +0800

    (packaging) Add distribution interface to the macOS package installer

--- packaging/pack.sh
+++ packaging/pack.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+set -euo pipefail
+
 BUNDLE_IDENTIFIER='org.jyutping.inputmethod.Jyutping'
 APP_VERSION='0.77.2'
 INSTALL_LOCATION='/Library/Input Methods'
@@ -10,8 +12,13 @@ pkgbuild \
     --identifier "${BUNDLE_IDENTIFIER}" \
     --version "${APP_VERSION}" \
     --install-location "${INSTALL_LOCATION}" \
-    --info PackageInfo \
     --component-plist JyutpingComponent.plist \
     --root "app" \
     --scripts "scripts" \
+    Jyutping-component.pkg
+
+productbuild \
+    --distribution distribution.xml \
+    --package-path . \
+    --resources resources \
     Jyutping.pkg
